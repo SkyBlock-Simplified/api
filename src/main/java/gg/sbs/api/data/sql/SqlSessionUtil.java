48cafdcee665cfc26be40d43c1f45096000aaf35
Nahydrin <brian@netcoding.net>
2021-10-06 16:49:52 -0400

    Clean up session factory building

--- src/main/java/gg/sbs/api/data/sql/SqlSessionUtil.java
+++ src/main/java/gg/sbs/api/data/sql/SqlSessionUtil.java
@@ -2,30 +2,11 @@ package gg.sbs.api.data.sql;
 
 import gg.sbs.api.SimplifiedApi;
 import gg.sbs.api.SimplifiedConfig;
-import gg.sbs.api.data.sql.driver.MariaDBDriver;
-import gg.sbs.api.data.sql.models.accessories.AccessoryModel;
-import gg.sbs.api.data.sql.models.accessoryfamilies.AccessoryFamilyModel;
-import gg.sbs.api.data.sql.models.collectionitems.CollectionItemModel;
-import gg.sbs.api.data.sql.models.collectionitemtiers.CollectionItemTierModel;
-import gg.sbs.api.data.sql.models.collections.CollectionModel;
-import gg.sbs.api.data.sql.models.enchantments.EnchantmentModel;
-import gg.sbs.api.data.sql.models.items.ItemModel;
-import gg.sbs.api.data.sql.models.itemtypes.ItemTypeModel;
-import gg.sbs.api.data.sql.models.pets.PetModel;
-import gg.sbs.api.data.sql.models.potions.PotionModel;
-import gg.sbs.api.data.sql.models.rarities.RarityModel;
-import gg.sbs.api.data.sql.models.reforges.ReforgeModel;
-import gg.sbs.api.data.sql.models.skilllevels.SkillLevelModel;
-import gg.sbs.api.data.sql.models.skills.SkillModel;
-import gg.sbs.api.util.FormatUtil;
-import gg.sbs.api.util.ResourceUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.ParameterizedType;
 import java.util.Properties;
 
 public class SqlSessionUtil {
@@ -33,49 +14,31 @@ public class SqlSessionUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
+        SimplifiedApi.enableDatabase();
         SimplifiedConfig config = SimplifiedApi.getConfig();
-        //Map<String, String> env = ResourceUtil.getEnvironmentVariables();
-        Properties properties = new Properties();
-        properties.putAll(
-                new HashMap<String, String>() {{
-                    put("connection.driver_class", SqlDrivers.MariaDB.getDriverClass() /*org.mariadb.jdbc.Driver.class.getCanonicalName()*/);
-                    put("hibernate.show_sql", config.getDatabaseDebug().toString());
-                    put("hibernate.connection.url", SqlDrivers.MariaDB.getConnectionUrl(config.getDatabaseHost(), config.getDatabasePort(), config.getDatabaseSchema())
-                            /*"jdbc:mariadb://" + env.get("DB_HOST")
-                            + ":" + env.get("DB_PORT")
-                            + "/" + env.get("DB_DATABASE")*/);
-                    put("hibernate.connection.username", config.getDatabaseUser());
-                    put("hibernate.connection.password", config.getDatabasePassword());
-                    put("hibernate.connection.provider_class",
-                            org.hibernate.hikaricp.internal.HikariCPConnectionProvider.class.getCanonicalName());
-                    put("hibernate.dialect", org.hibernate.dialect.MariaDB103Dialect.class.getCanonicalName());
-                    put("hikari.prepStmtCacheSize", "250");
-                    put("hikari.prepStmtCacheSqlLimit", "2048");
-                    put("hikari.cachePrepStmts", "true");
-                    put("hikari.useServerPrepStmts", "true");
-                }}
-        );
 
+        Properties properties = new Properties() {{
+            put("connection.driver_class", SqlDrivers.MariaDB.getDriverClass());
+            put("hibernate.show_sql", String.valueOf(config.isDatabaseDebugMode()));
+            put("hibernate.connection.url", SqlDrivers.MariaDB.getConnectionUrl(config.getDatabaseHost(), config.getDatabasePort(), config.getDatabaseSchema()));
+            put("hibernate.connection.username", config.getDatabaseUser());
+            put("hibernate.connection.password", config.getDatabasePassword());
+            put("hibernate.connection.provider_class", org.hibernate.hikaricp.internal.HikariCPConnectionProvider.class.getCanonicalName());
+            put("hibernate.dialect", org.hibernate.dialect.MariaDB103Dialect.class.getCanonicalName());
+            put("hikari.prepStmtCacheSize", "250");
+            put("hikari.prepStmtCacheSqlLimit", "2048");
+            put("hikari.cachePrepStmts", "true");
+            put("hikari.useServerPrepStmts", "true");
+        }};
+
+        // Add all inheritors of SqlModel
         Configuration configuration = new Configuration().setProperties(properties);
-        for (Class<? extends SqlModel> tClass : SimplifiedApi.getServiceManager().getServices(SqlModel.class))
+        for (Class<?> rClass : SimplifiedApi.getServiceManager().getServices(SqlRefreshable.class)) {
+            ParameterizedType superClass = (ParameterizedType) rClass.getGenericSuperclass();
+            Class<?> tClass = (Class<?>) superClass.getActualTypeArguments()[0];
             configuration = configuration.addAnnotatedClass(tClass);
+        }
 
-        /*for (Class<? extends SqlModel> model : Arrays.asList(
-                AccessoryModel.class,
-                AccessoryFamilyModel.class,
-                CollectionModel.class,
-                CollectionItemModel.class,
-                CollectionItemTierModel.class,
-                EnchantmentModel.class,
-                ItemModel.class,
-                ItemTypeModel.class,
-                PetModel.class,
-                PotionModel.class,
-                RarityModel.class,
-                ReforgeModel.class,
-                SkillModel.class,
-                SkillLevelModel.class
-        )) configuration = configuration.addAnnotatedClass(model);*/
         return configuration.buildSessionFactory();
     }
 
