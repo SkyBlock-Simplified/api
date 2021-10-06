68a9747eb3f6872b3ff536ac7fb41fc6089bb939
Nahydrin <brian@netcoding.net>
2021-10-06 16:44:39 -0400

    Added location areas classes

---
+++ src/main/java/gg/sbs/api/data/sql/models/locationareas/LocationAreaModel.java
@@ -0,0 +1,58 @@
+package gg.sbs.api.data.sql.models.locationareas;
+
+import gg.sbs.api.data.sql.SqlModel;
+import gg.sbs.api.data.sql.models.locations.LocationModel;
+import gg.sbs.api.data.sql.models.skills.SkillModel;
+import gg.sbs.api.util.builder.HashCodeBuilder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "locations")
+public class LocationAreaModel implements SqlModel {
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Getter
+    @Setter
+    @Column(name = "name", nullable = false, length = 127)
+    private String name;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "location", nullable = false)
+    private LocationModel location;
+
+    @Getter
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LocationAreaModel)) return false;
+
+        LocationAreaModel that = (LocationAreaModel) o;
+
+        if (id != that.id) return false;
+        if (!name.equals(that.name)) return false;
+        if (!location.equals(that.location)) return false;
+        return updatedAt.equals(that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this.id).append(this.name).append(this.location).append(this.updatedAt).build();
+    }
+
+}
---
+++ src/main/java/gg/sbs/api/data/sql/models/locationareas/LocationAreaRepository.java
@@ -0,0 +1,7 @@
+package gg.sbs.api.data.sql.models.locationareas;
+
+import gg.sbs.api.data.sql.SqlRepository;
+
+public class LocationAreaRepository extends SqlRepository<LocationAreaModel> {
+
+}
