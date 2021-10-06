38eb04e63bccaf7a2dae68c1ac2b0c9e96fe9e2a
Nahydrin <brian@netcoding.net>
2021-10-06 17:46:22 -0400

    Added minion tier upgrades table classes

---
+++ src/main/java/gg/sbs/api/data/sql/models/miniontierupgrades/MinionTierUpgradeModel.java
@@ -0,0 +1,69 @@
+package gg.sbs.api.data.sql.models.miniontierupgrades;
+
+import gg.sbs.api.data.sql.SqlModel;
+import gg.sbs.api.data.sql.models.collectionitems.CollectionItemModel;
+import gg.sbs.api.data.sql.models.miniontiers.MinionTierModel;
+import gg.sbs.api.util.builder.HashCodeBuilder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "minion_tier_upgrades")
+public class MinionTierUpgradeModel implements SqlModel {
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Getter
+    @Setter
+    @JoinColumn(name = "minion_tier", nullable = false)
+    private MinionTierModel minionTier;
+
+    @Getter
+    @Setter
+    @Column(name = "coin_cost", nullable = false)
+    private int coinCost;
+
+    @Getter
+    @Setter
+    @Column(name = "item_cost", length = 127)
+    private CollectionItemModel itemCost;
+
+    @Getter
+    @Setter
+    @Column(name = "quantity", nullable = false)
+    private int itemQuantity;
+
+    @Getter
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MinionTierUpgradeModel)) return false;
+
+        MinionTierUpgradeModel that = (MinionTierUpgradeModel) o;
+
+        if (id != that.id) return false;
+        if (!minionTier.equals(that.minionTier)) return false;
+        if (coinCost != that.coinCost) return false;
+        if (itemCost != that.itemCost) return false;
+        if (itemQuantity != that.itemQuantity) return false;
+        return updatedAt.equals(that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this.id).append(this.minionTier).append(this.coinCost).append(this.itemCost).append(this.itemQuantity).append(this.updatedAt).build();
+    }
+
+}
