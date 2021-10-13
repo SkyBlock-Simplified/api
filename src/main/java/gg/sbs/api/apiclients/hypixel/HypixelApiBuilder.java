58c67a1bca6d8bbfebc89eb785853e9de01aaceb
Nahydrin <brian@netcoding.net>
2021-10-13 01:02:51 -0400

    Use preconditions

--- src/main/java/gg/sbs/api/apiclients/hypixel/HypixelApiBuilder.java
+++ src/main/java/gg/sbs/api/apiclients/hypixel/HypixelApiBuilder.java
@@ -1,5 +1,6 @@
 package gg.sbs.api.apiclients.hypixel;
 
+import com.google.common.base.Preconditions;
 import gg.sbs.api.apiclients.ApiBuilder;
 import gg.sbs.api.apiclients.hypixel.implementation.HypixelDataInterface;
 import lombok.Getter;
@@ -10,22 +11,20 @@ import java.util.regex.Pattern;
 
 public final class HypixelApiBuilder extends ApiBuilder<HypixelDataInterface> {
 
+    public static final Pattern apiKeyRegex = Pattern.compile("[a-z0-9]{8}-(?:[a-z0-9]{4}-){3}[a-z0-9]{12}");
+    @Getter private String apiKey;
+
     public HypixelApiBuilder() {
         super("api.hypixel.net");
     }
 
-    public static final Pattern apiKeyRegex = Pattern.compile("[a-z0-9]{8}-(?:[a-z0-9]{4}-){3}[a-z0-9]{12}");
-    @Getter private String apiKey;
-
-    public void setApiKey(String newApiKey) {
-        if (newApiKey == null)
-            throw new IllegalArgumentException("New Hypixel API key must not be null");
-        else if (apiKeyRegex.matcher(newApiKey).matches())
-            throw new IllegalArgumentException("New Hypixel API key must be valid");
-
-        this.apiKey = newApiKey;
+    public void setApiKey(String apiKey) {
+        Preconditions.checkNotNull(apiKey, "Hypixel API key must not be NULL");
+        Preconditions.checkArgument(apiKeyRegex.matcher(apiKey).matches(), "Hypixel API key must be valid");
+        this.apiKey = apiKey;
     }
 
+    @Override
     public Map<String, String> buildHeaders() {
         return new HashMap<String, String>() {{
            put("API-Key", HypixelApiBuilder.this.getApiKey());
