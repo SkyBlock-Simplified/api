090f3b3b32c0370e00db7934f9709b1c3f6b6e90
Nahydrin <brian@netcoding.net>
2021-10-13 01:51:56 -0400

    Fixed my stupidity

--- src/main/java/gg/sbs/api/apiclients/hypixel/response/hypixel/HypixelErrorResponse.java
+++ src/main/java/gg/sbs/api/apiclients/hypixel/response/hypixel/HypixelErrorResponse.java
@@ -1,11 +1,12 @@
 package gg.sbs.api.apiclients.hypixel.response.hypixel;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 
 public class HypixelErrorResponse {
 
     @Getter private boolean success;
-    @SuppressWarnings("cause")
+    @SerializedName("cause")
     @Getter private String reason;
     @Getter private boolean throttle;
     @Getter private boolean global;
