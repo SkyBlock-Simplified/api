68add6fdd0e1a6e41fdfee6640f8b8317c39d373
Nahydrin <brian@netcoding.net>
2021-10-13 15:34:30 -0400

    Fix up HttpState and missing error codes

--- src/main/java/gg/sbs/api/apiclients/HttpState.java
+++ src/main/java/gg/sbs/api/apiclients/HttpState.java
@@ -1,14 +1,30 @@
 package gg.sbs.api.apiclients;
 
+import gg.sbs.api.util.StringUtil;
+import gg.sbs.api.util.WordUtil;
+import lombok.Getter;
+
 public enum HttpState {
 
 	CLIENT_ERROR,
 	JAVA_ERROR,
 	INFORMATIONAL,
 	OTHER,
+	NETWORK_ERROR,
 	REDIRECTION,
 	SERVER_ERROR,
-	CLOUDFLARE_ERROR,
-	SUCCESS
+	CLOUDFLARE_ERROR("CloudFlare"),
+	NGINX_ERROR("nginx"),
+	SUCCESS;
+
+	@Getter private final String title;
+
+	HttpState() {
+		this(null);
+	}
+
+	HttpState(String title) {
+		this.title = StringUtil.isEmpty(title) ? WordUtil.capitalizeFully(this.name().replace("_ERROR", "")) : title;
+	}
 
 }
--- src/main/java/gg/sbs/api/apiclients/HttpStatus.java
+++ src/main/java/gg/sbs/api/apiclients/HttpStatus.java
@@ -1,103 +1,141 @@
 package gg.sbs.api.apiclients;
 
+import gg.sbs.api.util.FormatUtil;
+import gg.sbs.api.util.StringUtil;
+import gg.sbs.api.util.WordUtil;
 import lombok.Getter;
 
 public enum HttpStatus {
 
-	CONTINUE(100, "Continue"),
-	SWITCHING_PROTOCOLS(101, "Switching Protocols"),
-	PROCESSING(102, "Processing"),
+	CONTINUE(100),
+	SWITCHING_PROTOCOLS(101),
+	PROCESSING(102),
+	EARLY_HINTS(103),
 	OK(200, "OK"),
-	CREATED(201, "Created"),
-	ACCEPTED(202, "Accepted"),
+	CREATED(201),
+	ACCEPTED(202),
 	NON_AUTHORITATIVE_INFORMATION(203, "Non-Authoritative Information"),
-	NO_CONTENT(204, "No Content"),
-	RESET_CONTENT(205, "Reset Content"),
-	PARTIAL_CONTENT(206, "Partial Content"),
+	NO_CONTENT(204),
+	RESET_CONTENT(205),
+	PARTIAL_CONTENT(206),
 	MULTI_STATUS(207, "Multi-Status"),
-	ALREADY_REPORTED(208, "Already Reported"),
+	ALREADY_REPORTED(208),
 	IM_USED(226, "IM Used"),
-	MULTIPLE_CHOICES(300, "Multiple Choices"),
-	MOVED_PERMANENTLY(301, "Moved Permanently"),
-	FOUND(302, "Found"),
-	SEE_OTHER(303, "See Other"),
-	NOT_MODIFIED(304, "Not Modified"),
-	USE_PROXY(305, "Use Proxy"),
-	SWITCH_PROXY(306, "Switch Proxy"),
-	TEMPORARY_REDIRECT(307, "Temporary Redirect"),
-	PERMANENT_REDIRECT(308, "Permanent Redirect"),
-	BAD_REQUEST(400, "Bad Request"),
-	UNAUTHORIZED(401, "Unauthorized"),
-	PAYMENT_REQUIRED(402, "Payment Required"),
-	FORBIDDEN(403, "Forbidden"),
-	NOT_FOUND(404, "Not Found"),
-	METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
-	NOT_ACCEPTABLE(406, "Not Acceptable"),
-	PROXY_AUTHENTICATION_REQUIRED(407, "Proxy Authentication Required"),
-	REQUEST_TIMEOUT(408, "Request Timeout"),
-	CONFLICT(409, "Conflict"),
-	GONE(410, "Gone"),
-	LENGTH_REQUIRED(411, "Length Required"),
-	PRECONDITION_FAILED(412, "Precondition Failed"),
-	REQUEST_ENTITY_TOO_LARGE(413, "Request Entity Too Large"),
+	MULTIPLE_CHOICES(300),
+	MOVED_PERMANENTLY(301),
+	FOUND(302),
+	SEE_OTHER(303),
+	NOT_MODIFIED(304),
+	USE_PROXY(305),
+	SWITCH_PROXY(306),
+	TEMPORARY_REDIRECT(307),
+	PERMANENT_REDIRECT(308),
+	BAD_REQUEST(400),
+	UNAUTHORIZED(401),
+	PAYMENT_REQUIRED(402),
+	FORBIDDEN(403),
+	NOT_FOUND(404),
+	METHOD_NOT_ALLOWED(405),
+	NOT_ACCEPTABLE(406),
+	PROXY_AUTHENTICATION_REQUIRED(407),
+	REQUEST_TIMEOUT(408),
+	CONFLICT(409),
+	GONE(410),
+	LENGTH_REQUIRED(411),
+	PRECONDITION_FAILED(412),
+	REQUEST_ENTITY_TOO_LARGE(413),
 	REQUEST_URI_TOO_LONG(414, "Request-URI Too Long"),
-	UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"),
-	REQUESTED_RANGE_NOT_SATISFIABLE(416, "Requested Range Not Satisfiable"),
-	EXPECTATION_FAILED(417, "Expectation Failed"),
+	UNSUPPORTED_MEDIA_TYPE(415),
+	REQUESTED_RANGE_NOT_SATISFIABLE(416),
+	EXPECTATION_FAILED(417),
 	IM_A_TEAPOT(418, "I'm a teapot"),
-	AUTHENTICATION_TIMEOUT(419, "Authentication Timeout"),
-	METHOD_FAILURE(420, "Method Failure"),
-	UNPROCESSABLE_ENTITY(422, "Unprocessable Entity"),
-	LOCKED(423, "Locked"),
-	FAILED_DEPENDENCY(424, "Failed Dependency"),
-	UPGRADE_REQUIRED(426, "Upgrade Required"),
-	PRECONDITION_REQUIRED(428, "Precondition Required"),
-	TOO_MANY_REQUESTS(429, "Too Many Requests"),
-	REQUEST_HEADER_FIELDS_TOO_LARGE(431, "Request Header Fields Too Large"),
-	LOGIN_TIMEOUT(440, "Login Timeout"),
-	NO_RESPONSE(444, "No Response"),
-	RETRY_WITH(449, "Retry With"),
-	BLOCKED_BY_WINDOWS_PARENTAL_CONTROLS(450, "Blocked by Windows Parental Controls"),
-	UNAVAILABLE_FOR_LEGAL_REASONS(451, "Unavailable For Legal Reasons"),
-	REDIRECT(451, "Redirect"),
-	REQUEST_HEADER_TOO_LARGE(494, "Request Header Too Large"),
-	CERT_ERROR(495, "Cert Error"),
-	NO_CERT(496, "No Cert"),
-	HTTP_TO_HTTPS(497, "HTTP to HTTPS"),
-	TOKEN_EXPIRED_INVALID(498, "Token expired/invalid"),
-	CLIENT_CLOSED_REQUEST(499, "Client Closed Request"),
-	INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
-	NOT_IMPLEMENTED(501, "Not Implemented"),
-	BAD_GATEWAY(502, "Bad Gateway"),
-	SERVICE_UNAVAILABLE(503, "Service Unavailable"),
-	GATEWAY_TIMEOUT(504, "Gateway Timeout"),
+	AUTHENTICATION_TIMEOUT(419),
+	METHOD_FAILURE(420),
+	MISDIRECTED_REQUEST(421),
+	UNPROCESSABLE_ENTITY(422),
+	LOCKED(423),
+	FAILED_DEPENDENCY(424),
+	TOO_EARLY(425),
+	UPGRADE_REQUIRED(426),
+	PRECONDITION_REQUIRED(428),
+	TOO_MANY_REQUESTS(429),
+	REQUEST_HEADER_FIELDS_TOO_LARGE(431),
+	LOGIN_TIMEOUT(440),
+	NO_RESPONSE(444, HttpState.NGINX_ERROR),
+	RETRY_WITH(449),
+	BLOCKED_BY_WINDOWS_PARENTAL_CONTROLS(450),
+	UNAVAILABLE_FOR_LEGAL_REASONS(451),
+	REQUEST_HEADER_TOO_LARGE(494, HttpState.NGINX_ERROR),
+	SSL_CERTIFICATE_ERROR(495, HttpState.NGINX_ERROR),
+	SSL_CERTIFICATE_REQUIRED(496, HttpState.NGINX_ERROR),
+	HTTP_REQUEST_SENT_TO_HTTPS(497, "HTTP Request Sent to HTTPS", HttpState.NGINX_ERROR),
+	CLIENT_CLOSED_REQUEST(499, HttpState.NGINX_ERROR),
+	INTERNAL_SERVER_ERROR(500),
+	NOT_IMPLEMENTED(501),
+	BAD_GATEWAY(502),
+	SERVICE_UNAVAILABLE(503),
+	GATEWAY_TIMEOUT(504),
 	HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported"),
-	VARIANT_ALSO_NEGOTIATES(506, "Variant Also Negotiate"),
-	INSUFFICIENT_STORAGE(507, "Insufficient Storage"),
-	LOOP_DETECTED(508, "Loop Detected"),
-	BANDWIDTH_LIMIT_EXCEEDED(509, "Bandwidth Limit Exceeded"),
-	NOT_EXTENDED(510, "Not Extended"),
-	NETWORK_AUTHENTICATION_REQUIRED(511, "Network Authentication Required"),
-	CLOUDFLARE_WEB_SERVER_UNKNOWN_ERROR(520, "CloudFlare: Web Server Returns An Unknown Error"),
-	CLOUDFLARE_WEB_SERVER_DOWN(521, "CloudFlare: Web Server Is Down"),
-	CLOUDFLARE_CONNECTION_TIMED_OUT(522, "CloudFlare: Connection Timed Out"),
-	CLOUDFLARE_ORIGIN_IS_UNREACHABLE(523, "CloudFlare: Origin Is Unreachable"),
-	CLOUDFLARE_A_TIME_OUT_OCCURRED(524, "CloudFlare: A Timeout Occurred"),
-	CLOUDFLARE_SSL_HANDSHAKE_FAILED(525, "CloudFlare: SSL Handshake Failed"),
-	CLOUDFLARE_INVALID_SSL_CERTIFICATE(526, "CloudFlare: Invalid SSL Certificate"),
-	CLOUDFLARE_RAILGUN_LISTENER_TO_ORIGIN_ERROR(527, "CloudFlare: Railgun Listener To Origin Error"),
-	CLOUDFLARE_GENERIC_ERROR(530, "CloudFlare: CloudFlare Generic Error"),
-	NETWORK_READ_TIMEOUT_ERROR(598, "Network read timeout error (Unknown)"),
-	NETWORK_CONNECT_TIMEOUT_ERROR(599, "Network connect timeout error (Unknown)"),
-	SOCKET_ERROR(990, "Socket Error"),
-	IO_ERROR(991, "IO Error"),
-	UNKNOWN_ERROR(999, "Unknown Error");
+	VARIANT_ALSO_NEGOTIATES(506),
+	INSUFFICIENT_STORAGE(507),
+	LOOP_DETECTED(508),
+	BANDWIDTH_LIMIT_EXCEEDED(509),
+	NOT_EXTENDED(510),
+	NETWORK_AUTHENTICATION_REQUIRED(511),
+	CLOUDFLARE_WEB_SERVER_UNKNOWN_ERROR(520, "Web Server Returns An Unknown Error", HttpState.CLOUDFLARE_ERROR),
+	CLOUDFLARE_WEB_SERVER_DOWN(521, "Web Server Is Down", HttpState.CLOUDFLARE_ERROR),
+	CLOUDFLARE_CONNECTION_TIMED_OUT(522, "Connection Timed Out", HttpState.CLOUDFLARE_ERROR),
+	CLOUDFLARE_ORIGIN_IS_UNREACHABLE(523, "Origin Is Unreachable", HttpState.CLOUDFLARE_ERROR),
+	CLOUDFLARE_A_TIME_OUT_OCCURRED(524, "A Timeout Occurred", HttpState.CLOUDFLARE_ERROR),
+	CLOUDFLARE_SSL_HANDSHAKE_FAILED(525, "SSL Handshake Failed", HttpState.CLOUDFLARE_ERROR),
+	CLOUDFLARE_INVALID_SSL_CERTIFICATE(526, "Invalid SSL Certificate", HttpState.CLOUDFLARE_ERROR),
+	CLOUDFLARE_RAILGUN_LISTENER_TO_ORIGIN_ERROR(527, "Railgun Listener To Origin Error", HttpState.CLOUDFLARE_ERROR),
+	CLOUDFLARE_GENERIC_ERROR(530, "Generic Error", HttpState.CLOUDFLARE_ERROR),
+	NETWORK_READ_TIMEOUT_ERROR(598, "Read Timeout Error", HttpState.NETWORK_ERROR),
+	NETWORK_CONNECT_TIMEOUT_ERROR(599, "Connect Timeout Error", HttpState.NETWORK_ERROR),
+	SOCKET_ERROR(990, HttpState.JAVA_ERROR),
+	IO_ERROR(991, "IO Error", HttpState.JAVA_ERROR),
+	UNKNOWN_ERROR(999, HttpState.JAVA_ERROR);
 
 	@Getter private final int code;
 	@Getter private final String message;
+	@Getter private final HttpState state;
+
+
+	HttpStatus(int code) {
+		this(code, null, null);
+	}
+
+	HttpStatus(int code, HttpState state) {
+		this(code, null, state);
+	}
 
 	HttpStatus(int code, String message) {
+		this(code, message, null);
+	}
+
+	HttpStatus(int code, String message, HttpState state) {
 		this.code = code;
+		message = StringUtil.isEmpty(message) ? WordUtil.capitalizeFully(this.name().replace("_", "")) : message;
+
+		if (state != null)
+			message = FormatUtil.format("{0}: {1}", state.getTitle(), message);
+		else {
+			if (this.getCode() >= 500)
+				state = HttpState.SERVER_ERROR;
+			else if (this.getCode() >= 400)
+				state = HttpState.CLIENT_ERROR;
+			else if (this.getCode() >= 300)
+				state = HttpState.REDIRECTION;
+			else if (this.getCode() >= 200)
+				state = HttpState.SUCCESS;
+			else if (this.getCode() >= 100)
+				state = HttpState.INFORMATIONAL;
+			else
+				state = HttpState.OTHER;
+		}
+
+		this.state = state;
 		this.message = message;
 	}
 
@@ -108,25 +146,6 @@ public enum HttpStatus {
 		}
 
 		return HttpStatus.OK;
-	}
-
-	public HttpState getState() {
-		if (this.getCode() >= 990)
-			return HttpState.JAVA_ERROR;
-		else if (this.getCode() >= 520 && this.getCode() <= 530)
-			return HttpState.CLOUDFLARE_ERROR;
-		else if (this.getCode() >= 500)
-			return HttpState.SERVER_ERROR;
-		else if (this.getCode() >= 400)
-			return HttpState.CLIENT_ERROR;
-		else if (this.getCode() >= 300)
-			return HttpState.REDIRECTION;
-		else if (this.getCode() >= 200)
-			return HttpState.SUCCESS;
-		else if (this.getCode() >= 100)
-			return HttpState.INFORMATIONAL;
-
-		return HttpState.OTHER;
 	}
 
 }
