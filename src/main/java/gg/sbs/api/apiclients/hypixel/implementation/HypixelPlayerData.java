ec2d182944e8f136517ddafef5582d76ba1b3b01
Nahydrin <brian@netcoding.net>
2021-10-13 01:02:17 -0400

    Remove headerMap and fix query parameters

--- src/main/java/gg/sbs/api/apiclients/hypixel/implementation/HypixelPlayerData.java
+++ src/main/java/gg/sbs/api/apiclients/hypixel/implementation/HypixelPlayerData.java
@@ -1,34 +1,35 @@
 package gg.sbs.api.apiclients.hypixel.implementation;
 
-import feign.HeaderMap;
 import feign.Param;
 import feign.RequestLine;
 import gg.sbs.api.apiclients.hypixel.response.hypixel.*;
 
-import java.util.Map;
 import java.util.UUID;
 
 public interface HypixelPlayerData extends HypixelDataInterface {
 
     @RequestLine("GET /counts")
-    HypixelCountsResponse getCounts(@HeaderMap Map<String, String> headerMap);
+    HypixelCountsResponse getCounts();
 
-    @RequestLine("GET /friends")
-    HypixelFriendsResponse getFriends(@HeaderMap Map<String, String> headerMap, @Param("uuid") UUID uniqueId);
+    @RequestLine("GET /friends?uuid={uuid}")
+    HypixelFriendsResponse getFriends(@Param("uuid") UUID uniqueId);
 
-    @RequestLine("GET /guild")
-    HypixelGuildResponse getGuild(@HeaderMap Map<String, String> headerMap, @Param("name") String name);
+    @RequestLine("GET /guild?id={id}")
+    HypixelGuildResponse getGuild(@Param("id") String guildId);
 
-    @RequestLine("GET /guild")
-    HypixelGuildResponse getGuild(@HeaderMap Map<String, String> headerMap, @Param("player") UUID uniqueId);
+    @RequestLine("GET /guild?name={name}")
+    HypixelGuildResponse getGuildByName(@Param("name") String guildName);
 
-    @RequestLine("GET /player")
-    HypixelPlayerResponse getPlayer(@HeaderMap Map<String, String> headerMap, @Param("uuid") UUID uniqueId);
+    @RequestLine("GET /guild?player={player}")
+    HypixelGuildResponse getGuildByPlayer(@Param("player") UUID uniqueId);
+
+    @RequestLine("GET /player?uuid={uuid}")
+    HypixelPlayerResponse getPlayer(@Param("uuid") UUID uniqueId);
 
     @RequestLine("GET /punishmentstats")
-    HypixelPunishmentStatsResponse getPunishmentStats(@HeaderMap Map<String, String> headerMap);
+    HypixelPunishmentStatsResponse getPunishmentStats();
 
-    @RequestLine("GET /status")
-    HypixelStatusResponse getStatus(@HeaderMap Map<String, String> headerMap, @Param("uuid") UUID uniqueId);
+    @RequestLine("GET /status?uuid={uuid}")
+    HypixelStatusResponse getStatus(@Param("uuid") UUID uniqueId);
 
 }
