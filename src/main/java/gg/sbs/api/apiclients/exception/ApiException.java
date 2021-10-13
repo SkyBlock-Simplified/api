6e78722ebac5ab3506fe42615f7cb7df0ce27fd3
Nahydrin <brian@netcoding.net>
2021-10-13 01:32:44 -0400

    Suppress deprecation on content call

--- src/main/java/gg/sbs/api/apiclients/exception/ApiException.java
+++ src/main/java/gg/sbs/api/apiclients/exception/ApiException.java
@@ -8,6 +8,7 @@ public abstract class ApiException extends FeignException {
 
     @Getter private final HttpStatus httpStatus;
 
+    @SuppressWarnings("deprecation")
     public ApiException(FeignException exception) {
         super(exception.status(), exception.getMessage(), exception.getCause(), exception.content(), exception.responseHeaders());
         this.httpStatus = HttpStatus.getByCode(this.status());
