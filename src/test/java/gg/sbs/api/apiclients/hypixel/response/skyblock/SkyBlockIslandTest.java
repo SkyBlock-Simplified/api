2f617ba30722bc3edb6dd817c46b088a692cfdec
Nahydrin <brian@netcoding.net>
2021-10-13 01:52:12 -0400

    Try catch for hypixel api

--- src/test/java/gg/sbs/api/apiclients/hypixel/response/skyblock/SkyBlockIslandTest.java
+++ src/test/java/gg/sbs/api/apiclients/hypixel/response/skyblock/SkyBlockIslandTest.java
@@ -1,6 +1,7 @@
 package gg.sbs.api.apiclients.hypixel.response.skyblock;
 
 import gg.sbs.api.SimplifiedApi;
+import gg.sbs.api.apiclients.exception.HypixelApiException;
 import gg.sbs.api.apiclients.hypixel.HypixelApiBuilder;
 import gg.sbs.api.apiclients.hypixel.implementation.HypixelSkyBlockData;
 import gg.sbs.api.util.StringUtil;
@@ -24,19 +25,23 @@ public class SkyBlockIslandTest {
 
     @Test
     public void getIsland_ok() {
-        UUID uniqueId = StringUtil.toUUID("f33f51a7-9691-4076-abda-f66e3d047a71"); // CraftedFury
-        //UUID uniqueId = StringUtil.toUUID("df5e1701-809c-48be-9b0d-ef50b83b009e"); // GoldenDusk
-        SkyBlockProfilesResponse profiles = hypixelSkyBlockData.getProfiles(uniqueId);
-        SkyBlockIsland island = profiles.getIslands().get(0);
-        Optional<SkyBlockIsland.Member> optionalMember = island.getMember(0);
+        try {
+            UUID uniqueId = StringUtil.toUUID("f33f51a7-9691-4076-abda-f66e3d047a71"); // CraftedFury
+            //UUID uniqueId = StringUtil.toUUID("df5e1701-809c-48be-9b0d-ef50b83b009e"); // GoldenDusk
+            SkyBlockProfilesResponse profiles = hypixelSkyBlockData.getProfiles(uniqueId);
+            SkyBlockIsland island = profiles.getIslands().get(0);
+            Optional<SkyBlockIsland.Member> optionalMember = island.getMember(0);
 
-        MatcherAssert.assertThat(profiles.isSuccess(), Matchers.equalTo(true));
-        MatcherAssert.assertThat(optionalMember.isPresent(), Matchers.equalTo(true));
+            MatcherAssert.assertThat(profiles.isSuccess(), Matchers.equalTo(true));
+            MatcherAssert.assertThat(optionalMember.isPresent(), Matchers.equalTo(true));
 
-        SkyBlockIsland.Member member = optionalMember.get();
-        //ConcurrentList<SkyBlockIsland.JacobsFarming.Contest> contests = member.getJacobsFarming().getContests();
-        ConcurrentList<SkyBlockIsland.PetInfo> pets = member.getPets();
-        MatcherAssert.assertThat(member.getUniqueId(), Matchers.equalTo(uniqueId));
+            SkyBlockIsland.Member member = optionalMember.get();
+            //ConcurrentList<SkyBlockIsland.JacobsFarming.Contest> contests = member.getJacobsFarming().getContests();
+            ConcurrentList<SkyBlockIsland.PetInfo> pets = member.getPets();
+            MatcherAssert.assertThat(member.getUniqueId(), Matchers.equalTo(uniqueId));
+        } catch (HypixelApiException exception) {
+            String error = "error";
+        }
     }
 
 }
