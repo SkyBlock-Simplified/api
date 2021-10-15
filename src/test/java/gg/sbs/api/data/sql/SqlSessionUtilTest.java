ae2ad2590d61cd63f6bc345947ccc425bab6d298
Nahydrin <brian@netcoding.net>
2021-10-15 16:25:01 -0400

    Make sure this test still works

--- src/test/java/gg/sbs/api/data/sql/SqlSessionUtilTest.java
+++ src/test/java/gg/sbs/api/data/sql/SqlSessionUtilTest.java
@@ -1,15 +1,19 @@
 package gg.sbs.api.data.sql;
 
+import gg.sbs.api.SimplifiedApi;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.hibernate.Session;
 import org.junit.jupiter.api.Test;
 
 public class SqlSessionUtilTest {
+
     @Test
     public void openSession_ok() {
-        Session session = SqlSessionUtil.openSession();
+        SimplifiedApi.enableDatabase();
+        Session session = SimplifiedApi.getSqlSession().openSession();
         MatcherAssert.assertThat(session, Matchers.notNullValue());
         session.close();
     }
+
 }
