06bee4368420c46220bda4932bddec427820ada4
Nahydrin <brian@netcoding.net>
2021-10-15 17:07:49 -0400

    Annotation reorder

--- src/main/java/gg/sbs/api/data/yaml/annotation/ConfigMode.java
+++ src/main/java/gg/sbs/api/data/yaml/annotation/ConfigMode.java
@@ -5,16 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ConfigMode {
 
 	Type value() default Type.DEFAULT;
 
 	enum Type {
+
 		DEFAULT,
 		FIELD_IS_KEY,
 		PATH_BY_UNDERSCORE
+
 	}
 
 }
--- src/main/java/gg/sbs/api/data/yaml/annotation/PreserveStatic.java
+++ src/main/java/gg/sbs/api/data/yaml/annotation/PreserveStatic.java
@@ -5,8 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PreserveStatic {
+
 	boolean value() default true;
+
 }
