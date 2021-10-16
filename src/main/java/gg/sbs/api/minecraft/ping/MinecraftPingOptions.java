74dfa4de683a38606f0da9ff3f34337208062513
Nahydrin <brian@netcoding.net>
2021-10-16 00:26:49 -0400

    Added server ping support

---
+++ src/main/java/gg/sbs/api/minecraft/ping/MinecraftPingOptions.java
@@ -0,0 +1,28 @@
+package gg.sbs.api.minecraft.ping;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Storage class for {@link MinecraftPing} options.
+ */
+@Builder
+public class MinecraftPingOptions {
+
+    @Getter private String hostname;
+
+    @Builder.Default
+    @Getter private Charset charset = StandardCharsets.UTF_8;
+
+    @Getter
+    @Builder.Default
+    private int port = 25565;
+
+    @Getter
+    @Builder.Default
+    private int timeout = 5000;
+
+}
