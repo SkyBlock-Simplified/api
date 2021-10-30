ffa6dd68d0da0fa68ec913b49b177bc0773a9ae2
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2021-10-30 19:33:51 -0400

    Removed dead api

--- src/main/java/dev/sbs/api/client/mojang/MojangServices.java
+++ src/main/java/dev/sbs/api/client/mojang/MojangServices.java
@@ -12,14 +12,16 @@ import java.util.UUID;
 public final class MojangServices {
 
 	// API: http://wiki.vg/Mojang_API
+
+	// Skin Renderer
 	// https://visage.surgeplay.com/index.html
+	// https://github.com/unascribed-archive/Visage
 	public static final URL SERVICE_MOJANG = getUrl("mojang.com");
 	public static final URL SERVICE_MOJANG_ACCOUNT = getUrl("account.mojang.com");
 	public static final URL SERVICE_MOJANG_API = getUrl("api.mojang.com");
 	public static final URL SERVICE_MOJANG_AUTH = getUrl("auth.mojang.com");
 	public static final URL SERVICE_MOJANG_AUTHSERVER = getUrl("authserver.mojang.com");
 	public static final URL SERVICE_MOJANG_SESSION = getUrl("sessionserver.mojang.com");
-	public static final URL SERVICE_MOJANG_STATUS = getUrl("status.mojang.com/check");
 	public static final URL SERVICE_MINECRAFT = getUrl("minecraft.net");
 	public static final URL SERVICE_MINECRAFT_SESSION = getUrl("session.minecraft.net");
 	public static final URL SERVICE_MINECRAFT_SKINS = getUrl("skins.minecraft.net");
