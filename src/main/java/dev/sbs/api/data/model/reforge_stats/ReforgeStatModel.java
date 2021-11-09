d99f145f29e4004a0f372574bbabb63c4e19fcef
Gabriel Pizarro <gpizarro@javaman.net>
2021-11-09 09:33:37 -0800

    Fix ReforgeStat linking to Reforge

--- src/main/java/dev/sbs/api/data/model/reforge_stats/ReforgeStatModel.java
+++ src/main/java/dev/sbs/api/data/model/reforge_stats/ReforgeStatModel.java
@@ -2,12 +2,13 @@ package dev.sbs.api.data.model.reforge_stats;
 
 import dev.sbs.api.data.model.Model;
 import dev.sbs.api.data.model.rarities.RarityModel;
+import dev.sbs.api.data.model.reforges.ReforgeModel;
 
 import java.util.Map;
 
 public interface ReforgeStatModel extends Model {
 
-    String getKey();
+    ReforgeModel getReforge();
 
     RarityModel getRarity();
 
