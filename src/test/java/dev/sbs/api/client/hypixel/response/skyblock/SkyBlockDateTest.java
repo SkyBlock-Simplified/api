60698bd0757abe783f3a495ba489fb964f99ad2e
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2021-11-29 14:49:43 -0500

    Add minute to date testing

--- src/test/java/dev/sbs/api/client/hypixel/response/skyblock/SkyBlockDateTest.java
+++ src/test/java/dev/sbs/api/client/hypixel/response/skyblock/SkyBlockDateTest.java
@@ -9,13 +9,14 @@ public class SkyBlockDateTest {
     @Test
     public void getDate_ok() {
         SkyBlockDate sbDate = new SkyBlockDate(System.currentTimeMillis(), true);
-        SkyBlockDate sbDate2 = new SkyBlockDate(sbDate.getYear(), sbDate.getMonth(), sbDate.getDay(), sbDate.getHour());
+        SkyBlockDate sbDate2 = new SkyBlockDate(sbDate.getYear(), sbDate.getMonth(), sbDate.getDay(), sbDate.getHour(), sbDate.getMinute());
 
         System.out.println("SB Time #1: " + sbDate.getSkyBlockTime());
         System.out.println("Year #1: " + sbDate.getYear());
         System.out.println("Month #1: " + sbDate.getMonth());
         System.out.println("Day #1: " + sbDate.getDay());
         System.out.println("Hour #1: " + sbDate.getHour());
+        System.out.println("Minute #1: " + sbDate.getMinute());
         System.out.println("Season #1: " + sbDate.getSeason().getName());
 
         System.out.println();
@@ -25,12 +26,14 @@ public class SkyBlockDateTest {
         System.out.println("Month #2: " + sbDate2.getMonth());
         System.out.println("Day #2: " + sbDate2.getDay());
         System.out.println("Hour #2: " + sbDate2.getHour());
+        System.out.println("Minute #2: " + sbDate2.getMinute());
         System.out.println("Season #2: " + sbDate2.getSeason().getName());
 
         MatcherAssert.assertThat(sbDate.getYear(), Matchers.equalTo(sbDate2.getYear()));
         MatcherAssert.assertThat(sbDate.getMonth(), Matchers.equalTo(sbDate2.getMonth()));
         MatcherAssert.assertThat(sbDate.getDay(), Matchers.equalTo(sbDate2.getDay()));
         MatcherAssert.assertThat(sbDate.getHour(), Matchers.equalTo(sbDate2.getHour()));
+        MatcherAssert.assertThat(sbDate.getMinute(), Matchers.equalTo(sbDate2.getMinute()));
     }
 
 }
