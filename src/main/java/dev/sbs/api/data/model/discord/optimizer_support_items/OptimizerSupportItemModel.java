9f6b3701f805e4c5b84f9c32d027b9eeb98303d1
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2021-12-07 02:10:13 -0500

    Add optimizer support items

---
+++ src/main/java/dev/sbs/api/data/model/discord/optimizer_support_items/OptimizerSupportItemModel.java
@@ -0,0 +1,10 @@
+package dev.sbs.api.data.model.discord.optimizer_support_items;
+
+import dev.sbs.api.data.model.EffectsModel;
+import dev.sbs.api.data.model.skyblock.items.ItemModel;
+
+public interface OptimizerSupportItemModel extends EffectsModel {
+
+    ItemModel getItem();
+
+}
---
+++ src/main/java/dev/sbs/api/data/model/discord/optimizer_support_items/OptimizerSupportItemSqlModel.java
@@ -0,0 +1,54 @@
+package dev.sbs.api.data.model.discord.optimizer_support_items;
+
+import dev.sbs.api.data.model.SqlModel;
+import dev.sbs.api.data.model.skyblock.items.ItemSqlModel;
+import dev.sbs.api.data.sql.converter.map.StringObjectMapConverter;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.Instant;
+import java.util.Map;
+
+@Entity
+@Table(
+    name = "discord_optimizer_support_items"
+)
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class OptimizerSupportItemSqlModel implements OptimizerSupportItemModel, SqlModel {
+
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Getter
+    @Setter
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
+    private ItemSqlModel item;
+
+    @Getter
+    @Setter
+    @Column(name = "effects", nullable = false)
+    @Convert(converter = StringObjectMapConverter.class)
+    private Map<String, Object> effects;
+
+    @Getter
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+    
+}
