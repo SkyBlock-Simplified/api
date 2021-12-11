c818251a493122935b585dd75e52a389173fdffe
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2021-12-10 23:31:46 -0500

    Separate effect and buff effect types

--- src/main/java/dev/sbs/api/data/model/skyblock/bonus_reforge_stats/BonusReforgeStatModel.java
+++ src/main/java/dev/sbs/api/data/model/skyblock/bonus_reforge_stats/BonusReforgeStatModel.java
@@ -3,7 +3,7 @@ package dev.sbs.api.data.model.skyblock.bonus_reforge_stats;
 import dev.sbs.api.data.model.BuffEffectsModel;
 import dev.sbs.api.data.model.skyblock.reforges.ReforgeModel;
 
-public interface BonusReforgeStatModel extends BuffEffectsModel<Double> {
+public interface BonusReforgeStatModel extends BuffEffectsModel<Object, Double> {
 
     ReforgeModel getReforge();
 
