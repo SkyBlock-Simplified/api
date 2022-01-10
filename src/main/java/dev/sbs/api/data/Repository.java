85ed0f23f6471b044806a8a5d589bb96770cfda6
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-01-10 03:34:41 -0500

    Large rewrite to add support for lambda boolean checks, expanded method support

--- src/main/java/dev/sbs/api/data/Repository.java
+++ src/main/java/dev/sbs/api/data/Repository.java
@@ -3,6 +3,7 @@ package dev.sbs.api.data;
 import dev.sbs.api.data.model.Model;
 import dev.sbs.api.data.sql.exception.SqlException;
 import dev.sbs.api.data.sql.function.FilterFunction;
+import dev.sbs.api.data.sql.function.TriFunction;
 import dev.sbs.api.util.concurrent.Concurrent;
 import dev.sbs.api.util.concurrent.ConcurrentList;
 import dev.sbs.api.util.helper.FormatUtil;
@@ -14,6 +15,7 @@ import lombok.NonNull;
 import java.lang.reflect.ParameterizedType;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 @SuppressWarnings("unchecked")
 public abstract class Repository<T extends Model> {
@@ -26,21 +28,7 @@ public abstract class Repository<T extends Model> {
         this.tClass = (Class<T>) superClass.getActualTypeArguments()[0];
     }
 
-    public abstract ConcurrentList<T> findAll();
-
-    public final <S> ConcurrentList<T> findAll(@NonNull FilterFunction<T, S> function, S value) throws SqlException {
-        return this.findAll(FilterFunction.Match.ALL, function, value);
-    }
-
-    public final <S> ConcurrentList<T> findAll(FilterFunction.Match match, @NonNull FilterFunction<T, S> function, S value) throws SqlException {
-        return this.findAll(match, Pair.of(function, value));
-    }
-
-    public final <S> ConcurrentList<T> findAll(@NonNull Pair<@NonNull FilterFunction<T, S>, S>... predicates) throws SqlException {
-        return this.findAll(FilterFunction.Match.ALL, predicates);
-    }
-
-    public final <S> ConcurrentList<T> findAll(FilterFunction.Match match, @NonNull Pair<@NonNull FilterFunction<T, S>, S>... predicates) throws SqlException {
+    private <S> ConcurrentList<T> compare(FilterFunction.Match match, TriFunction<FilterFunction<T, S>, T, S, Boolean> compare, Iterable<Pair<FilterFunction<T, S>, S>> predicates) throws SqlException {
         ConcurrentList<T> itemsCopy = this.findAll();
 
         if (ListUtil.notEmpty(itemsCopy)) {
@@ -50,16 +38,16 @@ public abstract class Repository<T extends Model> {
                     .filter(it -> {
                         boolean matches = false;
 
-                        for (Pair<FilterFunction<T, S>, S> pair : predicates)
-                            matches |= Objects.equals(pair.getKey().apply(it), pair.getValue());
+                        for (Pair<FilterFunction<T, S>, S> predicate : predicates)
+                            matches |= compare.apply(predicate.getLeft(), it, predicate.getValue());
 
                         return matches;
                     })
                     .collect(Concurrent.toList());
             } else if (match == FilterFunction.Match.ALL) {
-                for (Pair<FilterFunction<T, S>, S> pair : predicates) {
+                for (Pair<FilterFunction<T, S>, S> predicate : predicates) {
                     itemsCopy = itemsCopy.stream()
-                        .filter(it -> Objects.equals(pair.getKey().apply(it), pair.getValue()))
+                        .filter(it -> compare.apply(predicate.getLeft(), it, predicate.getRight()))
                         .collect(Concurrent.toList());
                 }
 
@@ -70,32 +58,138 @@ public abstract class Repository<T extends Model> {
             throw new SqlException(FormatUtil.format("Unable to load all items of type ''{0}''.", this.getTClass().getSimpleName()));
     }
 
-    public final <S> T findFirstOrNull(@NonNull FilterFunction<T, S> function, S value) throws SqlException {
-        return this.findFirst(function, value).orElse(null);
+    public abstract ConcurrentList<T> findAll();
+
+    public final <S> ConcurrentList<T> findAll(@NonNull FilterFunction<T, S> function, S value) throws SqlException {
+        return this.findAll(FilterFunction.Match.ALL, function, value);
+    }
+
+    public final <S> ConcurrentList<T> findAll(@NonNull Pair<FilterFunction<T, S>, S>... predicates) throws SqlException {
+        return this.findAll(Concurrent.newList(predicates));
+    }
+
+    public final <S> ConcurrentList<T> findAll(@NonNull Iterable<Pair<FilterFunction<T, S>, S>> predicates) throws SqlException {
+        return this.findAll(FilterFunction.Match.ALL, predicates);
+    }
+
+    public final <S> ConcurrentList<T> findAll(@NonNull FilterFunction.Match match, @NonNull FilterFunction<T, S> function, S value) throws SqlException {
+        return this.findAll(match, Pair.of(function, value));
+    }
+
+    public final <S> ConcurrentList<T> findAll(@NonNull FilterFunction.Match match, @NonNull Pair<FilterFunction<T, S>, S>... predicates) throws SqlException {
+        return this.findAll(match, Concurrent.newList(predicates));
+    }
+
+    public final <S> ConcurrentList<T> findAll(@NonNull FilterFunction.Match match, @NonNull Iterable<Pair<FilterFunction<T, S>, S>> predicates) throws SqlException {
+        return this.compare(
+            match,
+            (predicate, it, value) -> Objects.equals(predicate.apply(it), value),
+            predicates
+        );
     }
 
     public final <S> Optional<T> findFirst(@NonNull FilterFunction<T, S> function, S value) throws SqlException {
-        return this.findAll()
-            .stream()
-            .filter(model -> Objects.equals(function.apply(model), value))
-            .findFirst();
+        return this.findFirst(FilterFunction.Match.ALL, function, value);
     }
 
-    public final <S> T findFirstOrNull(@NonNull Pair<@NonNull FilterFunction<T, S>, S>... predicates) throws SqlException {
-        return this.findFirst(predicates).orElse(null);
-    }
-
-    public final <S> T findFirstOrNull(FilterFunction.Match match, @NonNull Pair<@NonNull FilterFunction<T, S>, S>... predicates) throws SqlException {
-        return this.findFirst(match, predicates).orElse(null);
+    public final <S> Optional<T> findFirst(@NonNull FilterFunction.Match match, @NonNull FilterFunction<T, S> function, S value) throws SqlException {
+        return this.findFirst(match, Pair.of(function, value));
     }
 
     public final <S> Optional<T> findFirst(@NonNull Pair<@NonNull FilterFunction<T, S>, S>... predicates) throws SqlException {
         return this.findFirst(FilterFunction.Match.ALL, predicates);
     }
 
-    public final <S> Optional<T> findFirst(FilterFunction.Match match, @NonNull Pair<@NonNull FilterFunction<T, S>, S>... predicates) throws SqlException {
+    public final <S> Optional<T> findFirst(@NonNull Iterable<Pair<@NonNull FilterFunction<T, S>, S>> predicates) throws SqlException {
+        return this.findFirst(FilterFunction.Match.ALL, predicates);
+    }
+
+    public final <S> Optional<T> findFirst(@NonNull FilterFunction.Match match, @NonNull Pair<FilterFunction<T, S>, S>... predicates) throws SqlException {
+        return this.findFirst(match, Concurrent.newList(predicates));
+    }
+
+    public final <S> Optional<T> findFirst(@NonNull FilterFunction.Match match, @NonNull Iterable<Pair<FilterFunction<T, S>, S>> predicates) throws SqlException {
         ConcurrentList<T> allMatches = this.findAll(match, predicates);
         return Optional.ofNullable(ListUtil.isEmpty(allMatches) ? null : allMatches.get(0));
+    }
+
+    public final <S> T findFirstOrNull(@NonNull FilterFunction<T, S> function, S value) throws SqlException {
+        return this.findFirst(function, value).orElse(null);
+    }
+
+    public final <S> T findFirstOrNull(@NonNull FilterFunction.Match match, @NonNull FilterFunction<T, S> function, S value) throws SqlException {
+        return this.findFirstOrNull(match, Pair.of(function, value));
+    }
+
+    public final <S> T findFirstOrNull(@NonNull FilterFunction.Match match, @NonNull Pair<FilterFunction<T, S>, S>... predicates) throws SqlException {
+        return this.findFirstOrNull(match, Concurrent.newList(predicates));
+    }
+
+    public final <S> T findFirstOrNull(@NonNull FilterFunction.Match match, @NonNull Iterable<Pair<FilterFunction<T, S>, S>> predicates) throws SqlException {
+        return this.findFirst(match, predicates).orElse(null);
+    }
+
+    public final <S> T findFirstOrNull(@NonNull Pair<@NonNull FilterFunction<T, S>, S>... predicates) throws SqlException {
+        return this.findFirstOrNull(Concurrent.newList(predicates));
+    }
+
+    public final <S> T findFirstOrNull(@NonNull Iterable<Pair<@NonNull FilterFunction<T, S>, S>> predicates) throws SqlException {
+        return this.findFirst(predicates).orElse(null);
+    }
+
+    public final ConcurrentList<T> matchAll(@NonNull FilterFunction<T, Boolean>... predicates) throws SqlException {
+        return this.matchAll(Concurrent.newList(predicates));
+    }
+
+    public final ConcurrentList<T> matchAll(@NonNull Iterable<FilterFunction<T, Boolean>> predicates) throws SqlException {
+        return this.matchAll(FilterFunction.Match.ALL, predicates);
+    }
+
+    public final ConcurrentList<T> matchAll(@NonNull FilterFunction.Match match, @NonNull FilterFunction<T, Boolean>... predicates) throws SqlException {
+        return this.matchAll(match, Concurrent.newList(predicates));
+    }
+
+    public final ConcurrentList<T> matchAll(@NonNull FilterFunction.Match match, @NonNull Iterable<FilterFunction<T, Boolean>> predicates) throws SqlException {
+        return this.compare(
+            match,
+            (predicate, it, value) -> predicate.apply(it),
+            StreamSupport.stream(predicates.spliterator(), false)
+                .map(predicate -> Pair.of(predicate, true))
+                .collect(Concurrent.toList())
+        );
+    }
+
+    public final Optional<T> matchFirst(@NonNull FilterFunction<T, Boolean>... predicates) throws SqlException {
+        return this.matchFirst(Concurrent.newList(predicates));
+    }
+
+    public final Optional<T> matchFirst(@NonNull Iterable<FilterFunction<T, Boolean>> predicates) throws SqlException {
+        return this.matchFirst(FilterFunction.Match.ALL, predicates);
+    }
+
+    public final Optional<T> matchFirst(@NonNull FilterFunction.Match match, @NonNull FilterFunction<T, Boolean>... predicates) throws SqlException {
+        return this.matchFirst(match, Concurrent.newList(predicates));
+    }
+
+    public final Optional<T> matchFirst(@NonNull FilterFunction.Match match, @NonNull Iterable<FilterFunction<T, Boolean>> predicates) throws SqlException {
+        ConcurrentList<T> allMatches = this.matchAll(match, predicates);
+        return Optional.ofNullable(ListUtil.isEmpty(allMatches) ? null : allMatches.get(0));
+    }
+
+    public final T matchFirstOrNull(@NonNull FilterFunction<T, Boolean>... predicates) throws SqlException {
+        return this.matchFirstOrNull(Concurrent.newList(predicates));
+    }
+
+    public final T matchFirstOrNull(@NonNull Iterable<FilterFunction<T, Boolean>> predicates) throws SqlException {
+        return this.matchFirstOrNull(FilterFunction.Match.ALL, predicates);
+    }
+
+    public final T matchFirstOrNull(@NonNull FilterFunction.Match match, @NonNull FilterFunction<T, Boolean>... predicates) throws SqlException {
+        return this.matchFirstOrNull(match, Concurrent.newList(predicates));
+    }
+
+    public final T matchFirstOrNull(@NonNull FilterFunction.Match match, @NonNull Iterable<FilterFunction<T, Boolean>> predicates) throws SqlException {
+        return this.matchFirst(match, predicates).orElse(null);
     }
 
 }
