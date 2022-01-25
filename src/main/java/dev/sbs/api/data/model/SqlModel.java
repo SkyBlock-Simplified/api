f01c5c21b4d410786ecc2fcaa1f439b56e1f68bc
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-01-24 20:51:42 -0500

    Add save/update methods

--- src/main/java/dev/sbs/api/data/model/SqlModel.java
+++ src/main/java/dev/sbs/api/data/model/SqlModel.java
@@ -5,13 +5,17 @@ import dev.sbs.api.data.sql.SqlRepository;
 
 import java.time.Instant;
 
+@SuppressWarnings("all")
 public interface SqlModel extends Model {
 
     Long getId();
 
     Instant getUpdatedAt();
 
-    @SuppressWarnings("all")
+    default void save() {
+        ((SqlRepository) SimplifiedApi.getRepositoryOf(this.getClass())).save(this);
+    }
+
     default void update() {
         ((SqlRepository) SimplifiedApi.getRepositoryOf(this.getClass())).update(this);
     }
