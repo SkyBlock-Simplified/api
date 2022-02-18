cdf0aca616a1c776f7d5ab219296389227a56be8
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-02-17 20:27:09 -0500

    Removed guild-level restrictions in global command configs

--- src/main/java/dev/sbs/api/data/model/discord/command_configs/CommandConfigModel.java
+++ src/main/java/dev/sbs/api/data/model/discord/command_configs/CommandConfigModel.java
@@ -3,19 +3,16 @@ package dev.sbs.api.data.model.discord.command_configs;
 import dev.sbs.api.data.model.Model;
 
 import java.time.Instant;
-import java.util.List;
 
 public interface CommandConfigModel extends Model {
 
     String getName();
 
-    boolean isBotOwnerOnly();
-
-    List<Long> getUsers();
-
-    List<Long> getRoles();
+    boolean isDeveloperOnly();
 
     boolean isEnabled();
+
+    boolean isGuildToggleable();
 
     String getStatus();
 
--- src/main/java/dev/sbs/api/data/model/discord/command_configs/CommandConfigSqlModel.java
+++ src/main/java/dev/sbs/api/data/model/discord/command_configs/CommandConfigSqlModel.java
@@ -1,7 +1,6 @@
 package dev.sbs.api.data.model.discord.command_configs;
 
 import dev.sbs.api.data.model.SqlModel;
-import dev.sbs.api.data.sql.converter.list.LongListConverter;
 import dev.sbs.api.util.builder.EqualsBuilder;
 import dev.sbs.api.util.builder.hashcode.HashCodeBuilder;
 import lombok.Getter;
@@ -12,14 +11,12 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.Instant;
-import java.util.List;
 
 @Entity
 @Table(
@@ -41,25 +38,18 @@ public class CommandConfigSqlModel implements CommandConfigModel, SqlModel {
 
     @Getter
     @Setter
-    @Column(name = "bot_owner", nullable = false)
-    private boolean botOwnerOnly;
-
-    @Getter
-    @Setter
-    @Column(name = "user_list", nullable = false)
-    @Convert(converter = LongListConverter.class)
-    private List<Long> users;
-
-    @Getter
-    @Setter
-    @Column(name = "role_list", nullable = false)
-    @Convert(converter = LongListConverter.class)
-    private List<Long> roles;
+    @Column(name = "developer_only", nullable = false)
+    private boolean developerOnly;
 
     @Getter
     @Setter
     @Column(name = "enabled", nullable = false)
     private boolean enabled;
+
+    @Getter
+    @Setter
+    @Column(name = "guild_toggleable", nullable = false)
+    private boolean guildToggleable;
 
     @Getter
     @Setter
@@ -79,16 +69,16 @@ public class CommandConfigSqlModel implements CommandConfigModel, SqlModel {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof CommandConfigSqlModel)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
+
         CommandConfigSqlModel that = (CommandConfigSqlModel) o;
 
         return new EqualsBuilder()
-            .append(this.isBotOwnerOnly(), that.isBotOwnerOnly())
+            .append(this.isDeveloperOnly(), that.isDeveloperOnly())
             .append(this.isEnabled(), that.isEnabled())
+            .append(this.isGuildToggleable(), that.isGuildToggleable())
             .append(this.getId(), that.getId())
             .append(this.getName(), that.getName())
-            .append(this.getUsers(), that.getUsers())
-            .append(this.getRoles(), that.getRoles())
             .append(this.getStatus(), that.getStatus())
             .append(this.getSubmittedAt(), that.getSubmittedAt())
             .append(this.getUpdatedAt(), that.getUpdatedAt())
@@ -100,10 +90,9 @@ public class CommandConfigSqlModel implements CommandConfigModel, SqlModel {
         return new HashCodeBuilder()
             .append(this.getId())
             .append(this.getName())
-            .append(this.isBotOwnerOnly())
-            .append(this.getUsers())
-            .append(this.getRoles())
+            .append(this.isDeveloperOnly())
             .append(this.isEnabled())
+            .append(this.isGuildToggleable())
             .append(this.getStatus())
             .append(this.getSubmittedAt())
             .append(this.getUpdatedAt())
