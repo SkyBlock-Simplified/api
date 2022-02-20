e5ca97228087b7d27781c63e8e88ddfebd6211da
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-02-19 23:38:01 -0500

    Some changes

--- src/main/java/dev/sbs/api/client/hypixel/response/skyblock/SkyBlockProfilesResponse.java
+++ src/main/java/dev/sbs/api/client/hypixel/response/skyblock/SkyBlockProfilesResponse.java
@@ -2,11 +2,15 @@ package dev.sbs.api.client.hypixel.response.skyblock;
 
 import com.google.gson.annotations.SerializedName;
 import dev.sbs.api.client.hypixel.response.skyblock.island.SkyBlockIsland;
+import dev.sbs.api.data.model.skyblock.profiles.ProfileModel;
 import dev.sbs.api.util.concurrent.Concurrent;
 import dev.sbs.api.util.concurrent.ConcurrentList;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SkyBlockProfilesResponse {
@@ -14,5 +18,12 @@ public class SkyBlockProfilesResponse {
     @Getter private boolean success;
     @SerializedName("profiles")
     @Getter private ConcurrentList<SkyBlockIsland> islands = Concurrent.newList();
+
+    public final Optional<SkyBlockIsland> getIsland(@NotNull ProfileModel profileModel) {
+        return this.getIslands()
+            .stream()
+            .filter(skyBlockIsland -> skyBlockIsland.getProfileName().map(profileModel::equals).orElse(false))
+            .findFirst();
+    }
 
 }
