62761a4337cd5f2a6c0c8652c03b611e5d64a14e
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-03-20 02:32:36 -0400

    Indexable stream support

--- src/main/java/dev/sbs/api/util/helper/StreamUtil.java
+++ src/main/java/dev/sbs/api/util/helper/StreamUtil.java
@@ -9,17 +9,23 @@ import dev.sbs.api.util.collection.concurrent.ConcurrentSet;
 import dev.sbs.api.util.collection.concurrent.linked.ConcurrentLinkedList;
 import dev.sbs.api.util.collection.concurrent.linked.ConcurrentLinkedMap;
 import dev.sbs.api.util.collection.search.function.TriFunction;
-import dev.sbs.api.util.data.tuple.Pair;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @SuppressWarnings("all")
 public class StreamUtil {
@@ -35,11 +41,96 @@ public class StreamUtil {
         return (key, value) -> { throw new IllegalStateException(FormatUtil.format("Duplicate key {0}!", key)); };
     }
 
-    public static <T> Stream<T> modifyStream(Stream<T> stringStream, TriFunction<Integer, T, Integer, T> modFunction) {
-        AtomicInteger counter = new AtomicInteger();
+    /**
+     * Returns a stream consisting of the results of applying the given function to the elements of
+     * {@code stream} and their indices in the stream. For example,
+     *
+     * <pre>{@code
+     * mapWithIndex(
+     *     Stream.of("a", "b", "c"),
+     *     (str, index, size) -> str + ":" + index + "/" + size)
+     * }</pre>
+     *
+     * <p>would return {@code Stream.of("a:0/3", "b:1/3", "c:2/3")}.
+     *
+     * <p>The resulting stream is <a
+     * href="http://gee.cs.oswego.edu/dl/html/StreamParallelGuidance.html">efficiently splittable</a>
+     * if and only if {@code stream} was efficiently splittable and its underlying spliterator
+     * reported {@link Spliterator#SUBSIZED}. This is generally the case if the underlying stream
+     * comes from a data structure supporting efficient indexed random access, typically an array or
+     * list.
+     *
+     * <p>The order of the resulting stream is defined if and only if the order of the original stream
+     * was defined.
+     */
+    public static <T, R> Stream<R> mapWithIndex(Stream<T> stream, TriFunction<? super T, Long, Long, ? extends R> function) {
+        Objects.requireNonNull(stream);
+        Objects.requireNonNull(function);
+        boolean isParallel = stream.isParallel();
+        Spliterator<T> fromSpliterator = stream.spliterator();
+        long size = fromSpliterator.estimateSize();
 
-        return stringStream.map(value -> Pair.of(counter.getAndIncrement(), value))
-            .map(pair -> modFunction.apply(pair.getKey(), pair.getValue(), counter.get()));
+        if (!fromSpliterator.hasCharacteristics(Spliterator.SUBSIZED)) {
+            Iterator<T> fromIterator = Spliterators.iterator(fromSpliterator);
+
+            return StreamSupport.stream(
+                    new Spliterators.AbstractSpliterator<R>(
+                        fromSpliterator.estimateSize(),
+                        fromSpliterator.characteristics() & (Spliterator.ORDERED | Spliterator.SIZED)) {
+                        long index = 0;
+
+                        @Override
+                        public boolean tryAdvance(Consumer<? super R> action) {
+                            if (fromIterator.hasNext()) {
+                                action.accept(function.apply(fromIterator.next(), index++, size));
+                                return true;
+                            }
+                            return false;
+                        }
+                    },
+                    isParallel)
+                .onClose(stream::close);
+        }
+
+        class Splitr extends MapWithIndexSpliterator<Spliterator<T>, R, Splitr> implements Consumer<T> {
+
+            private @Nullable T holder;
+
+            Splitr(Spliterator<T> splitr, long index) {
+                super(splitr, index);
+            }
+
+            @Override
+            public void accept(@NotNull T t) {
+                this.holder = t;
+            }
+
+            @Override
+            public boolean tryAdvance(Consumer<? super R> action) {
+                if (fromSpliterator.tryAdvance(this)) {
+                    try {
+                        // The cast is safe because tryAdvance puts a T into `holder`.
+                        action.accept(function.apply(holder, index++, size));
+                        return true;
+                    } finally {
+                        holder = null;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            Splitr createSplit(Spliterator<T> from, long i) {
+                return new Splitr(from, i);
+            }
+
+        }
+
+        return StreamSupport.stream(new Splitr(fromSpliterator, 0), isParallel).onClose(stream::close);
+    }
+
+    public static <T> Stream<T> modifyStream(Stream<T> stream, TriFunction<T, Long, Long, T> modFunction) {
+        return mapWithIndex(stream, (value, index, size) -> modFunction.apply(value, index, size));
     }
 
     public static Stream<String> appendEach(Stream<String> stringStream, String entryValue) {
@@ -47,7 +138,7 @@ public class StreamUtil {
     }
 
     public static Stream<String> appendEach(Stream<String> stringStream, String entryValue, String lastEntry) {
-        return modifyStream(stringStream, (index, value, size) -> value + (index < size - 1 ? entryValue : lastEntry));
+        return modifyStream(stringStream, (value, index, size) -> value + (index < size - 1 ? entryValue : lastEntry));
     }
 
     public static Stream<String> prependEach(Stream<String> stringStream, String entryValue) {
@@ -55,7 +146,7 @@ public class StreamUtil {
     }
 
     public static Stream<String> prependEach(Stream<String> stringStream, String entryValue, String lastEntry) {
-        return modifyStream(stringStream, (index, value, size) -> (index < size - 1 ? entryValue : lastEntry) + value);
+        return modifyStream(stringStream, (value, index, size) -> (index < size - 1 ? entryValue : lastEntry) + value);
     }
 
     public static <E> Collector<E, ?, ConcurrentCollection<E>> toConcurrentCollection() {
@@ -71,8 +162,8 @@ public class StreamUtil {
     }
 
     public static <T, K, V> Collector<T, ?, ConcurrentMap<K, V>> toConcurrentMap() {
-        Function<? super T, ? extends K> keyMapper = entry -> ((Map.Entry<K, V>)entry).getKey();
-        Function<? super T, ? extends V> valueMapper = entry -> ((Map.Entry<K, V>)entry).getValue();
+        Function<? super T, ? extends K> keyMapper = entry -> ((Map.Entry<K, V>) entry).getKey();
+        Function<? super T, ? extends V> valueMapper = entry -> ((Map.Entry<K, V>) entry).getValue();
         return toConcurrentMap(keyMapper, valueMapper);
     }
 
@@ -94,8 +185,8 @@ public class StreamUtil {
     }
 
     public static <T, K, V> Collector<T, ?, ConcurrentLinkedMap<K, V>> toConcurrentLinkedMap() {
-        Function<? super T, ? extends K> keyMapper = entry -> ((Map.Entry<K, V>)entry).getKey();
-        Function<? super T, ? extends V> valueMapper = entry -> ((Map.Entry<K, V>)entry).getValue();
+        Function<? super T, ? extends K> keyMapper = entry -> ((Map.Entry<K, V>) entry).getKey();
+        Function<? super T, ? extends V> valueMapper = entry -> ((Map.Entry<K, V>) entry).getValue();
         return toConcurrentLinkedMap(keyMapper, valueMapper);
     }
 
@@ -183,6 +274,44 @@ public class StreamUtil {
         @Override
         public Supplier<A> supplier() {
             return this.supplier;
+        }
+
+    }
+
+    private abstract static class MapWithIndexSpliterator<F extends Spliterator<?>, R extends Object, S extends MapWithIndexSpliterator<F, R, S>> implements Spliterator<R> {
+
+        protected final F fromSpliterator;
+        protected long index;
+
+        MapWithIndexSpliterator(F fromSpliterator, long index) {
+            this.fromSpliterator = fromSpliterator;
+            this.index = index;
+        }
+
+        abstract S createSplit(F from, long i);
+
+        @Override
+        public S trySplit() {
+            Spliterator<?> splitOrNull = fromSpliterator.trySplit();
+
+            if (splitOrNull == null) {
+                return null;
+            }
+
+            F split = (F) splitOrNull;
+            S result = createSplit(split, index);
+            this.index += split.getExactSizeIfKnown();
+            return result;
+        }
+
+        @Override
+        public long estimateSize() {
+            return fromSpliterator.estimateSize();
+        }
+
+        @Override
+        public int characteristics() {
+            return fromSpliterator.characteristics() & (Spliterator.ORDERED | Spliterator.SIZED | Spliterator.SUBSIZED);
         }
 
     }
