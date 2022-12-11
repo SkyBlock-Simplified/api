430be32fa0be92a6cd7b6cd1c8637aa4f1ecb462
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-12-11 14:26:17 -0500

    Fix getLastPlayed

--- src/main/java/dev/sbs/api/client/hypixel/response/skyblock/SkyBlockProfilesResponse.java
+++ src/main/java/dev/sbs/api/client/hypixel/response/skyblock/SkyBlockProfilesResponse.java
@@ -5,14 +5,14 @@ import dev.sbs.api.client.hypixel.response.skyblock.island.SkyBlockIsland;
 import dev.sbs.api.data.model.skyblock.profiles.ProfileModel;
 import dev.sbs.api.util.collection.concurrent.Concurrent;
 import dev.sbs.api.util.collection.concurrent.ConcurrentList;
-import dev.sbs.api.util.date.CustomDate;
+import dev.sbs.api.util.collection.search.function.SearchFunction;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Comparator;
 import java.util.Optional;
-import java.util.UUID;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SkyBlockProfilesResponse {
@@ -28,34 +28,11 @@ public class SkyBlockProfilesResponse {
             .findFirst();
     }
 
-    public SkyBlockIsland getLastPlayed(UUID uniqueId) {
-        Optional<SkyBlockIsland> lastPlayedIsland = Optional.empty();
-        long lastPlayed = 0;
-
-        for (SkyBlockIsland skyBlockIsland : this.getIslands()) {
-            if (!lastPlayedIsland.isPresent()) {
-                lastPlayedIsland = Optional.of(skyBlockIsland);
-                lastPlayed = skyBlockIsland.getMember(uniqueId)
-                    .map(SkyBlockIsland.Member::getLastSave)
-                    .map(SkyBlockDate::getRealTime)
-                    .orElse(0L);
-                continue;
-            }
-
-            Optional<SkyBlockIsland.Member> optionalMember = skyBlockIsland.getMember(uniqueId);
-
-            if (optionalMember.isPresent()) {
-                SkyBlockIsland.Member member = optionalMember.get();
-                long memberLastPlayed = Optional.ofNullable(member.getLastSave()).map(CustomDate::getRealTime).orElse(-1L);
-
-                if (memberLastPlayed > lastPlayed) {
-                    lastPlayedIsland = Optional.of(skyBlockIsland);
-                    lastPlayed = memberLastPlayed;
-                }
-            }
-        }
-
-        return lastPlayedIsland.orElse(this.getIslands().get(0));
+    public SkyBlockIsland getLastPlayed() {
+        return this.getIslands()
+            .stream()
+            .max(Comparator.comparing(SearchFunction.combine(SkyBlockIsland::getLastSave, SkyBlockDate.RealTime::getRealTime)))
+            .orElse(this.getIslands().get(0));
     }
 
 }
