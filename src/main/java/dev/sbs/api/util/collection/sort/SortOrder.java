7a9a8682f933e816872f7ae1b763db301116e63d
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-01-31 15:44:25 -0500

    Maybe working order button

--- src/main/java/dev/sbs/api/util/collection/sort/SortOrder.java
+++ src/main/java/dev/sbs/api/util/collection/sort/SortOrder.java
@@ -1,8 +1,20 @@
 package dev.sbs.api.util.collection.sort;
 
+import dev.sbs.api.util.helper.WordUtil;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@RequiredArgsConstructor
 public enum SortOrder {
 
-    ASCENDING,
-    DESCENDING
+    ASCENDING("ASC"),
+    DESCENDING("DESC");
+
+    @Getter private final @NotNull String shortName;
+
+    public String getName() {
+        return WordUtil.capitalizeFully(this.name());
+    }
 
 }
