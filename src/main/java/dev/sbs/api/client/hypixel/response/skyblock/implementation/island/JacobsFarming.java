35d3b6a842bfcebb992c6ac20d4e14a01d864b26
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-02-03 00:53:24 -0500

    Make these classes non-optional

---
+++ src/main/java/dev/sbs/api/client/hypixel/response/skyblock/implementation/island/JacobsFarming.java
@@ -0,0 +1,120 @@
+package dev.sbs.api.client.hypixel.response.skyblock.implementation.island;
+
+import com.google.gson.annotations.SerializedName;
+import dev.sbs.api.SimplifiedApi;
+import dev.sbs.api.client.hypixel.response.skyblock.implementation.SkyBlockDate;
+import dev.sbs.api.data.model.skyblock.collection_data.collection_items.CollectionItemModel;
+import dev.sbs.api.util.collection.concurrent.Concurrent;
+import dev.sbs.api.util.collection.concurrent.ConcurrentList;
+import dev.sbs.api.util.collection.concurrent.ConcurrentMap;
+import dev.sbs.api.util.collection.concurrent.ConcurrentSet;
+import dev.sbs.api.util.helper.ListUtil;
+import dev.sbs.api.util.helper.NumberUtil;
+import dev.sbs.api.util.helper.StringUtil;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+public class JacobsFarming {
+
+    @SerializedName("medals_inv")
+    @Getter private ConcurrentMap<Medal, Integer> medalInventory = Concurrent.newMap();
+    @Getter private ConcurrentMap<Perk, Integer> perks = Concurrent.newMap();
+    @SerializedName("unique_golds2")
+    private ConcurrentSet<String> uniqueGolds = Concurrent.newSet();
+    private ConcurrentMap<String, Contest> contests = Concurrent.newMap();
+    private ConcurrentList<Contest> contestData = Concurrent.newList();
+    private boolean talked;
+
+    public ConcurrentList<Contest> getContests() {
+        if (ListUtil.isEmpty(this.contestData)) {
+            this.contests.forEach(entry -> {
+                Contest contest = entry.getValue();
+
+                String[] dataString = entry.getKey().split(":");
+                String[] calendarString = dataString[1].split("_");
+                int year = NumberUtil.toInt(dataString[0]);
+                int month = NumberUtil.toInt(calendarString[0]);
+                int day = NumberUtil.toInt(calendarString[1]);
+                String collectionName = StringUtil.join(dataString, ":", 2, dataString.length);
+
+                contest.skyBlockDate = new SkyBlockDate(year, month, day);
+                contest.collectionName = collectionName;
+                this.contestData.add(contest);
+            });
+
+            this.contestData = Concurrent.newUnmodifiableList(this.contestData);
+        }
+
+        return this.contestData;
+    }
+
+    public int getMedals(Medal medal) {
+        return this.getMedalInventory().getOrDefault(medal, 0);
+    }
+
+    public int getPerk(Perk perk) {
+        return this.getPerks().getOrDefault(perk, 0);
+    }
+
+    public ConcurrentSet<CollectionItemModel> getUniqueGolds() {
+        return SimplifiedApi.getRepositoryOf(CollectionItemModel.class)
+            .stream()
+            .filter(collectionItem -> uniqueGolds.contains(collectionItem.getItem().getItemId()))
+            .collect(Concurrent.toSet());
+    }
+
+    public boolean hasTalked() {
+        return this.talked;
+    }
+
+    public enum Medal {
+
+        @SerializedName("bronze")
+        BRONZE,
+        @SerializedName("silver")
+        SILVER,
+        @SerializedName("gold")
+        GOLD
+
+    }
+
+    public enum Perk {
+
+        @SerializedName("double_drops")
+        DOUBLE_DROPS,
+        @SerializedName("farming_level_cap")
+        FARMING_LEVEL_CAP
+
+    }
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Contest {
+
+        @Getter private int collected;
+        @SerializedName("claimed_rewards")
+        private boolean claimedRewards;
+        @SerializedName("claimed_position")
+        @Getter private int position;
+        @SerializedName("claimed_participants")
+        @Getter private int participants;
+
+        @Getter private SkyBlockDate skyBlockDate;
+        @Getter private String collectionName;
+
+        public boolean hasClaimedRewards() {
+            return this.claimedRewards;
+        }
+
+        @NoArgsConstructor(access = AccessLevel.PRIVATE)
+        public static class Data {
+
+            @Getter private SkyBlockDate skyBlockDate;
+            @Getter private String collectionName;
+
+        }
+
+    }
+
+}
---
+++ src/main/java/dev/sbs/api/client/hypixel/response/skyblock/implementation/island/MelodyHarp.java
@@ -0,0 +1,64 @@
+package dev.sbs.api.client.hypixel.response.skyblock.implementation.island;
+
+import dev.sbs.api.client.hypixel.response.skyblock.implementation.SkyBlockDate;
+import dev.sbs.api.util.collection.concurrent.Concurrent;
+import dev.sbs.api.util.collection.concurrent.ConcurrentMap;
+import dev.sbs.api.util.collection.concurrent.linked.ConcurrentLinkedMap;
+import dev.sbs.api.util.data.tuple.Pair;
+import dev.sbs.api.util.helper.FormatUtil;
+import dev.sbs.api.util.helper.NumberUtil;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+public class MelodyHarp {
+
+    @Getter private final boolean talismanClaimed;
+    @Getter private final String selectedSong;
+    @Getter private final SkyBlockDate.RealTime selectedSongTimestamp;
+    @Getter private final ConcurrentMap<String, Song> songs;
+
+    MelodyHarp(ConcurrentMap<String, Object> harpQuest) {
+        this.talismanClaimed = (boolean) harpQuest.removeOrGet("claimed_talisman", false);
+        this.selectedSong = (String) harpQuest.removeOrGet("selected_song", "");
+        long epoch = NumberUtil.createNumber(String.valueOf(harpQuest.removeOrGet("selected_song_epoch", 0))).longValue();
+        this.selectedSongTimestamp = new SkyBlockDate.RealTime(epoch * 1000) ;
+
+        ConcurrentLinkedMap<String, ConcurrentMap<String, Integer>> songMap = Concurrent.newLinkedMap();
+        harpQuest.forEach((harpKey, harpValue) -> {
+            if (harpValue instanceof Number) {
+                String songKey = harpKey.replace("song_", "");
+                String songName = songKey.replaceAll("_((best|perfect)_)?completions?", "");
+                String category = songKey.replace(FormatUtil.format("{0}_", songName), "");
+
+                if (!songMap.containsKey(songName))
+                    songMap.put(songName, Concurrent.newMap());
+
+                songMap.get(songName).put(category, NumberUtil.createNumber(harpValue.toString()).intValue());
+            }
+        });
+
+        this.songs = Concurrent.newUnmodifiableMap(
+            songMap.stream()
+                .map(entry -> Pair.of(
+                    entry.getKey(),
+                    new Song(
+                        entry.getValue().getOrDefault("best_completion", 0),
+                        entry.getValue().getOrDefault("completions", 0),
+                        entry.getValue().getOrDefault("perfect_completions", 0)
+                    )
+                ))
+                .collect(Concurrent.toMap())
+        );
+    }
+
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Song {
+
+        @Getter private final int bestCompletion;
+        @Getter private final int completions;
+        @Getter private final int perfectCompletions;
+
+    }
+
+}
