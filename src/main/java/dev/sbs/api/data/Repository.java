4a0a6ceb563cdb17c539ef6259e45aef4700f775
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-02-04 18:49:36 -0500

    Require model class through constructor

--- src/main/java/dev/sbs/api/data/Repository.java
+++ src/main/java/dev/sbs/api/data/Repository.java
@@ -2,30 +2,28 @@ package dev.sbs.api.data;
 
 import dev.sbs.api.data.exception.DataException;
 import dev.sbs.api.data.model.Model;
-import dev.sbs.api.reflection.Reflection;
 import dev.sbs.api.util.collection.concurrent.Concurrent;
 import dev.sbs.api.util.collection.concurrent.ConcurrentList;
 import dev.sbs.api.util.collection.search.SearchQuery;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.stream.Stream;
 
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 public abstract class Repository<T extends Model> implements SearchQuery<T, ConcurrentList<T>> {
 
-    @Getter private final Class<T> tClass;
-
-    public Repository() {
-        this.tClass = Reflection.getSuperClass(this);
-    }
+    @Getter private final @NotNull Class<T> type;
 
     @Override
-    public final ConcurrentList<T> findAll() throws DataException {
+    public final @NotNull ConcurrentList<T> findAll() throws DataException {
         return this.stream().collect(Concurrent.toList());
     }
 
     @Override
-    public final ConcurrentList<T> toList(@NotNull Stream<T> stream) throws DataException {
+    public final @NotNull ConcurrentList<T> toList(@NotNull Stream<T> stream) throws DataException {
         return stream.collect(Concurrent.toList());
     }
 
