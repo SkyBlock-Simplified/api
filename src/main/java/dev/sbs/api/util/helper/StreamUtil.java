31f63fe8fea6ae6f9e953d7702bf95b9128ab53f
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-05-19 16:41:14 -0400

    Speed up concurrent collectors

--- src/main/java/dev/sbs/api/util/helper/StreamUtil.java
+++ src/main/java/dev/sbs/api/util/helper/StreamUtil.java
@@ -161,15 +161,15 @@ public class StreamUtil {
     }
 
     public static <E> Collector<E, ?, ConcurrentCollection<E>> toConcurrentCollection() {
-        return new StreamCollector<>(ConcurrentCollection::new, ConcurrentCollection::add, (left, right) -> { left.addAll(right); return left; }, CHARACTERISTICS);
+        return new StreamCollector<>(ConcurrentCollection::new, ConcurrentCollection::addAll, (left, right) -> { left.addAll(right); return left; }, CHARACTERISTICS);
     }
 
     public static <E> Collector<E, ?, ConcurrentLinkedList<E>> toConcurrentLinkedList() {
-        return new StreamCollector<>(ConcurrentLinkedList::new, ConcurrentLinkedList::add, (left, right) -> { left.addAll(right); return left; }, CHARACTERISTICS);
+        return new StreamCollector<>(ConcurrentLinkedList::new, ConcurrentLinkedList::addAll, (left, right) -> { left.addAll(right); return left; }, CHARACTERISTICS);
     }
 
     public static <E> Collector<E, ?, ConcurrentList<E>> toConcurrentList() {
-        return new StreamCollector<>(ConcurrentList::new, ConcurrentList::add, (left, right) -> { left.addAll(right); return left; }, CHARACTERISTICS);
+        return new StreamCollector<>(ConcurrentList::new, ConcurrentList::addAll, (left, right) -> { left.addAll(right); return left; }, CHARACTERISTICS);
     }
 
     public static <T, K, V> Collector<T, ?, ConcurrentMap<K, V>> toConcurrentMap() {
