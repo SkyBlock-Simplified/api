f135b05705e763a6c8345b10470e1bf1da0ea54b
Nahydrin <6407029+CraftedFury@users.noreply.github.com>
2023-07-10 02:10:23 -0400

    Update some nodes

--- src/main/java/dev/sbs/api/client/hypixel/response/skyblock/implementation/island/Leveling.java
+++ src/main/java/dev/sbs/api/client/hypixel/response/skyblock/implementation/island/Leveling.java
@@ -11,14 +11,16 @@ import lombok.NoArgsConstructor;
 public class Leveling {
 
     @Getter private int experience;
-    @SerializedName("migrated_completions")
-    @Getter private boolean migratedCompletions;
     @SerializedName("category_expanded")
     @Getter private boolean categoriesExpanded;
-    @SerializedName("completed")
-    private ConcurrentList<String> completedTasks = Concurrent.newList();
     @SerializedName("last_viewed_tasks")
-    private ConcurrentList<String> lastViewedTasks = Concurrent.newList();
+    @Getter private ConcurrentList<String> lastViewedTasks = Concurrent.newList();
+    @SerializedName("completed_tasks")
+    @Getter private ConcurrentList<String> completedTasks = Concurrent.newList();
+    @SerializedName("highest_pet_score")
+    @Getter private int highestPetScore;
+    @SerializedName("mining_fiesta_ores_mined")
+    @Getter private int miningFiestaOresMined;
 
     public int getLevel() {
         return (int) Math.floor(this.getExperience() / 100.0);
