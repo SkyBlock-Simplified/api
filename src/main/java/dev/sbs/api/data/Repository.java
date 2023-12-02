f38da3b136350fe37dd8171e6adc716f5ca895ec
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2023-12-02 13:10:40 -0500

    Add repository caching

--- src/main/java/dev/sbs/api/data/Repository.java
+++ src/main/java/dev/sbs/api/data/Repository.java
@@ -5,21 +5,33 @@ import dev.sbs.api.data.model.Model;
 import dev.sbs.api.util.collection.concurrent.Concurrent;
 import dev.sbs.api.util.collection.concurrent.ConcurrentList;
 import dev.sbs.api.util.collection.search.SearchQuery;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Hibernate;
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.ResultSet;
 import java.util.stream.Stream;
 
-@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+@RequiredArgsConstructor
 public abstract class Repository<T extends Model> implements SearchQuery<T, ConcurrentList<T>> {
 
-    @Getter private final @NotNull Class<T> type;
+    @Getter protected final @NotNull Class<T> type;
+    protected @NotNull ConcurrentList<T> cache = Concurrent.newList();
 
+    /**
+     * Returns an updated {@link ResultSet} from {@link Hibernate} collected in {@link Stream<T>}.
+     */
     @Override
-    public final @NotNull ConcurrentList<T> findAll() throws DataException {
-        return this.stream().collect(Concurrent.toList());
+    public final @NotNull Stream<T> stream() throws DataException {
+        return this.findAll().stream();
+    }
+
+    /**
+     * Returns the cached {@link ResultSet} from {@link Hibernate} collected in {@link ConcurrentList<T>}.
+     */
+    public final @NotNull ConcurrentList<T> findCached() {
+        return this.cache;
     }
 
     @Override
