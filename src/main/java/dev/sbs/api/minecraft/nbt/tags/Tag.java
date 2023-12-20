12a22cbdf9a146c127d3dd3637a94b367d182e9c
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2023-12-20 02:50:23 -0500

    Cleanup nbt tags, fix incorrect comparison methods, remove required use of TagRegistry

--- src/main/java/dev/sbs/api/minecraft/nbt/tags/Tag.java
+++ src/main/java/dev/sbs/api/minecraft/nbt/tags/Tag.java
@@ -1,69 +1,79 @@
 package dev.sbs.api.minecraft.nbt.tags;
 
-import dev.sbs.api.minecraft.nbt.json.JsonSerializable;
-import dev.sbs.api.minecraft.nbt.registry.TagTypeRegistry;
-import dev.sbs.api.minecraft.nbt.snbt.SnbtConfig;
-import dev.sbs.api.minecraft.nbt.snbt.SnbtSerializable;
+import dev.sbs.api.minecraft.nbt.serializable.json.JsonSerializable;
+import dev.sbs.api.minecraft.nbt.serializable.snbt.SnbtSerializable;
+import dev.sbs.api.minecraft.nbt.serializable.tag.TagSerializable;
+import dev.sbs.api.util.builder.hash.EqualsBuilder;
+import dev.sbs.api.util.builder.hash.HashCodeBuilder;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * An abstract NBT tag.
  */
-@AllArgsConstructor
-public abstract class Tag<T> implements SnbtSerializable, JsonSerializable {
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class Tag<T> implements TagSerializable, JsonSerializable, SnbtSerializable {
 
     /**
-     * Returns the name (key) of this tag.
+     * Gets the unique ID for this NBT tag type.
+     * <br><br>
+     * 0 to 12 (inclusive) are reserved.
      */
-    @Getter
-    protected String name;
+    private final byte typeId;
     /**
-     * Returns the value held by this tag.
+     * Gets the name (key) of this tag.
      */
-    @Getter
-    @NotNull
-    protected T value;
-    @Setter
-    protected TagTypeRegistry registry;
-    private boolean updatable;
+    private @Nullable String name;
+    /**
+     * Gets the value held by this tag.
+     */
+    private @NotNull T value;
+    /**
+     * Gets if the name and value are modifiable.
+     */
+    @Getter(AccessLevel.PROTECTED)
+    private boolean modifiable;
 
-    /**
-     * Returns a unique ID for this NBT tag type. 0 to 12 (inclusive) are reserved.
-     *
-     * @return a unique ID for this NBT tag type.
-     */
-    public abstract byte getTypeId();
+    @Override
+    public final boolean equals(@Nullable Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Tag<?> that = (Tag<?>) obj;
 
-    /**
-     * Reads this tag from a {@link DataInput} stream.
-     *
-     * @param input    the stream to read from.
-     * @param depth    the current depth of the NBT data structure.
-     * @param registry the {@link TagTypeRegistry} to be used in reading.
-     * @return this (literally {@code return this;} after reading).
-     * @throws IOException if any I/O error occurs.
-     */
-    public abstract Tag<?> read(DataInput input, int depth, TagTypeRegistry registry) throws IOException;
+        return new EqualsBuilder()
+            .append(this.getTypeId(), that.getTypeId())
+            .append(this.getName(), that.getName())
+            .append(this.getValue(), that.getValue())
+            .append(this.isModifiable(), that.isModifiable())
+            .build();
+    }
+
+    @Override
+    public final int hashCode() {
+        return new HashCodeBuilder()
+            .append(this.getTypeId())
+            .append(this.getName())
+            .append(this.getValue())
+            .build();
+    }
+
+    protected final void requireModifiable() {
+        if (!this.isModifiable())
+            throw new UnsupportedOperationException("This Tag is not modifiable!");
+    }
 
     /**
      * Sets the name (key) of this tag.
      *
      * @param name the new name to be set.
      */
-    public final void setName(String name) {
-        if (this.updatable)
-            this.name = name;
-    }
-
-    protected final void setNonUpdatable() {
-        this.updatable = false;
+    public final void setName(@Nullable String name) {
+        this.requireModifiable();
+        this.name = name;
     }
 
     /**
@@ -72,23 +82,13 @@ public abstract class Tag<T> implements SnbtSerializable, JsonSerializable {
      * @param value new {@link T} value to be set.
      */
     public final void setValue(@NotNull T value) {
-        if (this.updatable)
-            this.value = value;
+        this.requireModifiable();
+        this.value = value;
     }
 
-    /**
-     * Writes this tag to a {@link DataOutput} stream.
-     *
-     * @param output   the stream to write to.
-     * @param depth    the current depth of the NBT data structure.
-     * @param registry the {@link TagTypeRegistry} to be used in writing.
-     * @throws IOException if any I/O error occurs.
-     */
-    public abstract void write(DataOutput output, int depth, TagTypeRegistry registry) throws IOException;
-
     @Override
-    public final String toString() {
-        return this.toSnbt(0, new TagTypeRegistry(), new SnbtConfig());
+    public final @NotNull String toString() {
+        return this.getValue().toString();
     }
 
 }
