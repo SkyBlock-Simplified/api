9472e26c746f30a9a518d72ca8ed2384b91e3530
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2023-12-20 03:44:14 -0500

    Keep all mojang error fields

--- src/main/java/dev/sbs/api/client/mojang/exception/MojangErrorResponse.java
+++ src/main/java/dev/sbs/api/client/mojang/exception/MojangErrorResponse.java
@@ -3,18 +3,23 @@ package dev.sbs.api.client.mojang.exception;
 import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 
+@Getter
 public class MojangErrorResponse {
 
-    @Getter protected boolean success;
+    protected boolean success;
+    @SerializedName("error")
+    protected String type;
     @SerializedName("errorMessage")
-    @Getter protected String reason;
+    protected String reason;
     protected String path;
 
     public static class Unknown extends MojangErrorResponse {
 
         public Unknown() {
             super.success = false;
-            super.reason = "Unknown";
+            super.type = "UNKNOWN";
+            super.reason = "Unknown Reason";
+            super.path = "";
         }
 
     }
