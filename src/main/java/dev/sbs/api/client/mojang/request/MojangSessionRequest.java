ec19174f197d96b3b44363489210d97de7f682cd
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2023-12-20 03:42:31 -0500

    Get signature value from responses

--- src/main/java/dev/sbs/api/client/mojang/request/MojangSessionRequest.java
+++ src/main/java/dev/sbs/api/client/mojang/request/MojangSessionRequest.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 
 public interface MojangSessionRequest extends IMojangRequest {
 
-    @RequestLine("GET /session/minecraft/profile/{uniqueId}")
+    @RequestLine("GET /session/minecraft/profile/{uniqueId}?unsigned=false")
     @NotNull MojangPropertiesResponse getProperties(@NotNull @Param("uniqueId") UUID uniqueId) throws MojangApiException;
 
 }
