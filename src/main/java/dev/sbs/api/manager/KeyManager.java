8b9f6ebb8b85f87b236607443fee2bd43996bf07
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2023-12-22 19:54:43 -0500

    Expose the parent methods

--- src/main/java/dev/sbs/api/manager/KeyManager.java
+++ src/main/java/dev/sbs/api/manager/KeyManager.java
@@ -1,5 +1,8 @@
 package dev.sbs.api.manager;
 
+import dev.sbs.api.manager.exception.InsufficientModeException;
+import dev.sbs.api.manager.exception.RegisteredReferenceException;
+import dev.sbs.api.manager.exception.UnknownReferenceException;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
@@ -18,6 +21,26 @@ public class KeyManager<K, V> extends Manager<K, V> {
 
     public KeyManager(@NotNull BiFunction<Map.Entry<K, V>, K, Boolean> keyMatcher, @NotNull Mode mode) {
         super(keyMatcher, mode);
+    }
+
+    @Override
+    public final void add(@NotNull K identifier, @NotNull V value) throws RegisteredReferenceException {
+        super.add(identifier, value);
+    }
+
+    @Override
+    public final @NotNull V get(@NotNull K identifier) throws UnknownReferenceException {
+        return super.get(identifier);
+    }
+
+    @Override
+    public final void remove(@NotNull K identifier) throws InsufficientModeException, UnknownReferenceException {
+        super.remove(identifier);
+    }
+
+    @Override
+    public final void update(@NotNull K identifier, @NotNull V newValue) throws InsufficientModeException, UnknownReferenceException {
+        super.update(identifier, newValue);
     }
 
 }
