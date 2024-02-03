e86ba27f29fb96b3e260db4b43e7f03095fa2763
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2024-02-03 04:59:00 -0500

    Change these to DataStreams so they don't need to be wrapped

--- src/main/java/dev/sbs/api/util/io/ByteArrayDataOutput.java
+++ src/main/java/dev/sbs/api/util/io/ByteArrayDataOutput.java
@@ -3,179 +3,66 @@ package dev.sbs.api.util.io;
 import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutput;
+import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 
 /**
- * An extension of {@code DataOutput} for writing to in-memory byte arrays; its methods offer
- * identical functionality but do not throw {@link IOException}.
+ * An extension of {@link java.io.DataOutputStream} for writing to in-memory byte arrays.
  */
-public interface ByteArrayDataOutput extends DataOutput {
+public class ByteArrayDataOutput extends DataOutputStream {
 
-    @Override
-    void write(int b);
+    public ByteArrayDataOutput() {
+        this(32);
+    }
 
-    @Override
-    void write(byte @NotNull [] b);
+    public ByteArrayDataOutput(int size) {
+        this(new java.io.ByteArrayOutputStream(size));
+    }
 
-    @Override
-    void write(byte @NotNull [] b, int off, int len);
+    public ByteArrayDataOutput(@NotNull java.io.ByteArrayOutputStream outputStream) {
+        super(outputStream);
+    }
 
-    @Override
-    void writeBoolean(boolean v);
+    private @NotNull java.io.ByteArrayOutputStream getUnderlying() {
+        return (java.io.ByteArrayOutputStream) super.out;
+    }
 
-    @Override
-    void writeByte(int v);
-
-    @Override
-    void writeShort(int v);
-
-    @Override
-    void writeChar(int v);
-
-    @Override
-    void writeInt(int v);
-
-    @Override
-    void writeLong(long v);
-
-    @Override
-    void writeFloat(float v);
-
-    @Override
-    void writeDouble(double v);
-
-    @Override
-    void writeChars(@NotNull String s);
-
-    @Override
-    void writeUTF(@NotNull String s);
-
-    /**
-     * @deprecated This method is dangerous as it discards the high byte of every character. For
-     * UTF-8, use {@code write(s.getBytes(StandardCharsets.UTF_8))}.
-     */
-    @Deprecated
-    @Override
-    void writeBytes(@NotNull String s);
+    public void reset() {
+        this.getUnderlying().reset();
+    }
 
     /**
      * Returns the contents that have been written to this instance, as a byte array.
      */
-    byte[] toByteArray();
-
-    static @NotNull ByteArrayDataOutput of() {
-        return of(new ByteArrayOutputStream());
+    public byte[] toByteArray() {
+        return this.getUnderlying().toByteArray();
     }
 
-    static @NotNull ByteArrayDataOutput of(int size) {
-        return of(new ByteArrayOutputStream(size));
+    @Override
+    public String toString() {
+        return this.getUnderlying().toString();
     }
 
-    static @NotNull ByteArrayDataOutput of(@NotNull ByteArrayOutputStream byteArrayOutputStream) {
-        return new ByteArrayDataOutput.Impl(byteArrayOutputStream);
+    @SneakyThrows
+    public String toString(@NotNull String charsetName) {
+        return this.getUnderlying().toString(charsetName);
     }
 
-    class Impl implements ByteArrayDataOutput {
+    public String toString(@NotNull Charset charset) {
+        return this.getUnderlying().toString(charset);
+    }
 
-        private final @NotNull DataOutput output;
-        private final @NotNull ByteArrayOutputStream byteArrayOutputStream;
+    public void writeImage(@NotNull RenderedImage renderedImage, @NotNull String formatName) throws IOException {
+        ImageIO.write(renderedImage, formatName, this);
+    }
 
-        Impl(@NotNull ByteArrayOutputStream byteArrayOutputStream) {
-            this.byteArrayOutputStream = byteArrayOutputStream;
-            this.output = new DataOutputStream(byteArrayOutputStream);
-        }
-
-        @SneakyThrows
-        @Override
-        public void write(int b) {
-            this.output.write(b);
-        }
-
-        @SneakyThrows
-        @Override
-        public void write(byte @NotNull [] b) {
-            this.output.write(b);
-        }
-
-        @SneakyThrows
-        @Override
-        public void write(byte @NotNull [] b, int off, int len) {
-            this.output.write(b, off, len);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeBoolean(boolean v) {
-            this.output.writeBoolean(v);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeByte(int v) {
-            this.output.writeByte(v);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeBytes(@NotNull String s) {
-            this.output.writeBytes(s);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeChar(int v) {
-            this.output.writeChar(v);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeChars(@NotNull String s) {
-            this.output.writeChars(s);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeDouble(double v) {
-            this.output.writeDouble(v);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeFloat(float v) {
-            this.output.writeFloat(v);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeInt(int v) {
-            this.output.writeInt(v);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeLong(long v) {
-            this.output.writeLong(v);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeShort(int v) {
-            this.output.writeShort(v);
-        }
-
-        @SneakyThrows
-        @Override
-        public void writeUTF(@NotNull String s) {
-            this.output.writeUTF(s);
-        }
-
-        @Override
-        public byte[] toByteArray() {
-            return this.byteArrayOutputStream.toByteArray();
-        }
+    @SneakyThrows
+    public void writeTo(@NotNull OutputStream outputStream) {
+        this.getUnderlying().writeTo(outputStream);
     }
 
 }
