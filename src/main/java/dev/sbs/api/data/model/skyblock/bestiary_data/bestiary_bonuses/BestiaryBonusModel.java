ad0864c081c74d7857f8b2333936078441a2253d
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-06-22 02:26:14 -0400

    Migrated files to simplified-bot module

--- src/main/java/dev/sbs/api/data/model/skyblock/bestiary_data/bestiary_bonuses/BestiaryBonusModel.java
+++
@@ -1,9 +0,0 @@
-package dev.sbs.api.data.model.skyblock.bestiary_data.bestiary_bonuses;
-
-import dev.sbs.api.data.model.EffectsModel;
-
-public interface BestiaryBonusModel extends EffectsModel<Double> {
-
-    Integer getLevel();
-
-}
