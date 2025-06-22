cbacdf9c24ce036f0b31cad18c454bbb3bb426a2
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-06-22 02:27:29 -0400

    Migrated files to minecraft-api module

--- src/main/java/dev/sbs/api/client/impl/hypixel/response/skyblock/implementation/island/util/weight/Weighted.java
+++
@@ -1,9 +0,0 @@
-package dev.sbs.api.client.impl.hypixel.response.skyblock.implementation.island.util.weight;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface Weighted {
-
-    @NotNull Weight getWeight();
-
-}
--- src/main/java/dev/sbs/api/client/impl/mojang/request/IMojangRequest.java
+++
@@ -1,7 +0,0 @@
-package dev.sbs.api.client.impl.mojang.request;
-
-import dev.sbs.api.client.request.IRequest;
-
-public interface IMojangRequest extends IRequest {
-
-}
