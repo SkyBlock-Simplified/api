0fc04f956b418b392a2e6eeae085b6e6ad13d92a
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-09-05 15:04:50 -0400

    Phrasing

--- src/main/java/dev/sbs/api/collection/search/SearchFunction.java
+++ src/main/java/dev/sbs/api/collection/search/SearchFunction.java
@@ -11,10 +11,10 @@ public interface SearchFunction<T, R> extends Function<T, R> {
      * Alows you to traverse down through method references.
      *
      * @param from  The first function in the chain
-     * @param to The last function in the chain
-     * @param <T1>   The first class to traverse
-     * @param <T2>   The second class to traverse
-     * @param <T3>   The return type
+     * @param to    The next function in the chain
+     * @param <T1>  The first class to traverse
+     * @param <T2>  The next class to traverse
+     * @param <T3>  The return type
      * @return The method reference between {@link T1} and {@link T3}
      */
     static <T1, T2, T3> @NotNull SearchFunction<T1, T3> combine(@NotNull SearchFunction<T1, T2> from, @NotNull SearchFunction<T2, T3> to) {
