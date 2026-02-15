ca1739721d051f75f1ca9734baf836f690517c17
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-02-15 14:06:43 -0500

    Request now lives in Response, and thus doesn't need timestamp

--- src/main/java/dev/sbs/api/client/request/Request.java
+++ src/main/java/dev/sbs/api/client/request/Request.java
@@ -6,30 +6,37 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.Instant;
-
 public interface Request {
 
+    /**
+     * Retrieves the headers of the request.
+     *
+     * @return a {@link ConcurrentMap} where the keys are the header names and the values
+     *         are lists of the corresponding header values.
+     */
     @NotNull ConcurrentMap<String, ConcurrentList<String>> getHeaders();
 
+    /**
+     * Retrieves the HTTP method associated with the request.
+     *
+     * @return the {@link HttpMethod} of the request
+     */
     @NotNull HttpMethod getMethod();
 
-    @NotNull Instant getTimestamp();
-
+    /**
+     * Retrieves the URL associated with the request.
+     *
+     * @return the request URL
+     */
     @NotNull String getUrl();
 
     @Getter
     @RequiredArgsConstructor
     class Impl implements Request {
 
-        private final long timestamp;
         private final @NotNull HttpMethod method;
         private final @NotNull String url;
         private final @NotNull ConcurrentMap<String, ConcurrentList<String>> headers;
-
-        public @NotNull Instant getTimestamp() {
-            return Instant.ofEpochMilli(this.timestamp);
-        }
 
     }
 
